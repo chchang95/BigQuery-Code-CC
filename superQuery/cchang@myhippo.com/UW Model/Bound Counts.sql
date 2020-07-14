80d3b0fc6f4992d125b6038365bacdf9af9bd267
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-14 10:49:44 -0700

    Auto-commit: Tue Jul 14 2020 10:49:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/UW Model/Bound Counts.sql
+++ superQuery/cchang@myhippo.com/UW Model/Bound Counts.sql
@@ -201,7 +201,7 @@ SELECT
     SUM(tot_bound_premium) AS tot_bound_premium
 FROM 
     converted_policies
-    where Bind_Date >= '2020-04-30'
+    where Bind_Date >= '2019-01-01'
     and product <> 'HO5'
     and carrier <> 'Canopius'
 GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
