5e4ca386222bb17d2b5031cd66f7e159f7914c2b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-14 17:19:30 -0700

    Auto-commit: Tue Jul 14 2020 17:19:24 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
+++ superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
@@ -1,9 +1,10 @@
 select 
         carrier
+        , case when update_action in ('terminate', 'revert_terminate', 'reinstate') then 'return' else 'new' end as trans_type
         , greatest(date_trunc(date_customer_update_made, MONTH), date_trunc(date_update_effective, MONTH), date_report_period_start) as date_accounting_start
-        ,sum(written_total) as written_prem_x_ebsl
+        ,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
 from dw_prod_extracts.ext_policy_update_monthly_premiums epud
 where carrier = 'Canopius'
 and date_knowledge = '2020-06-30'
-group by 1,2
-order by 1,2
+group by 1,2,3
+order by 1,2,3
