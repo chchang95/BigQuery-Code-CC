352647bd8ffb9cfd6ec5d13f83e584a3989f8b02
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-14 17:12:39 -0700

    Auto-commit: Tue Jul 14 2020 17:12:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
+++ superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
@@ -8,6 +8,6 @@
         -- , date_sub(date_add(greatest(date_trunc(date_customer_update_made, MONTH), date_trunc(date_update_effective, MONTH), date_report_period_start), INTERVAL 1 MONTH), INTERVAL 1 DAY) as date_accounting_end
         ,sum(written_total) as written_prem_x_ebsl
 from dw_prod_extracts.ext_policy_update_monthly_premiums epud
-where carrier = 'Canopius'
+where carrier = 'Spinnaker'
 group by 1,2
 order by 1,2
