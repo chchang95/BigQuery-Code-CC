9c0aeaea3a4d5dca2004905afd0d58eb25da8969
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-20 16:18:37 -0700

    Auto-commit: Mon Jul 20 2020 16:18:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/BuildFax Sample - 0630.sql
+++ superQuery/cchang@myhippo.com/Other/BuildFax Sample - 0630.sql
@@ -1,3 +1,5 @@
+with policy_claims as (
+# 13338
 select distinct policy_id, policy_number
 , property_data_address_street as street
 , property_data_address_city as city
@@ -10,3 +12,75 @@ where date_snapshot = '2020-06-30'
 and claims.policy_id is not null
 and carrier <> 'Canopius'
 and product <> 'HO5'
+), policy_referrals as (
+# 1432
+select policy_id, policy_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'r' as type
+from dw_prod_extracts.ext_policy_snapshots
+where date_snapshot = '2020-06-30'
+and carrier <> 'Canopius'
+and state not in ('TX', 'CA')
+and product <> 'HO5'
+and calculated_fields_non_cat_risk_class = 'referral'
+), policy_interior as (
+# 1665 (25%)
+select policy_id, policy_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'i' as type
+from dw_prod_extracts.ext_policy_snapshots
+where date_snapshot = '2020-06-30'
+and carrier <> 'Canopius'
+and state not in ('TX', 'CA')
+and product <> 'HO5'
+and calculated_fields_non_cat_risk_class = 'interior_inspection_required'
+order by street asc
+LIMIT 1665
+), policy_exterior as (
+# 1665 (25%)
+select policy_id, policy_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'e' as type
+from dw_prod_extracts.ext_policy_snapshots
+where date_snapshot = '2020-06-30'
+and carrier <> 'Canopius'
+and state not in ('TX', 'CA')
+and product <> 'HO5'
+and calculated_fields_non_cat_risk_class = 'exterior_inspection_required'
+order by street asc
+LIMIT 1665
+), policy_no_action as (
+# 1900 (25%)
+select policy_id, policy_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'h' as type
+from dw_prod_extracts.ext_policy_snapshots
+where date_snapshot = '2020-06-30'
+and carrier <> 'Canopius'
+and state not in ('TX', 'CA')
+and product <> 'HO5'
+and calculated_fields_non_cat_risk_class = 'no_action'
+order by street asc
+LIMIT 1900
+)
+select * from policy_claims
+union all
+select * from policy_referrals
+union all
+select * from policy_interior
+union all
+select * from policy_exterior
+union all
+select * from policy_no_action
