3911ed525766f4aeedbde3a9cf0588b9c831449b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-20 16:04:22 -0700

    Auto-commit: Mon Jul 20 2020 16:04:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -1,5 +1,3 @@
-
-# 1086 (25%)
 select policy_id, policy_number
 , property_data_address_street as street
 , property_data_address_city as city
@@ -10,4 +8,4 @@ where date_snapshot = '2020-06-30'
 and carrier <> 'Canopius'
 and state not in ('TX', 'CA')
 and product <> 'HO5'
-and calculated_fields_non_cat_risk_class = 'no_action'
+and calculated_fields_non_cat_risk_class = 'referral'
