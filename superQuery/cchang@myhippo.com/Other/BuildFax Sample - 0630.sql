d639651b7746c120f14fd9296cc68024e225f66a
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-20 16:16:52 -0700

    Auto-commit: Mon Jul 20 2020 16:16:46 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Other/BuildFax Sample - 0630.sql
@@ -0,0 +1,12 @@
+select distinct policy_id, policy_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'c' as type
+from dw_prod_extracts.ext_policy_snapshots
+left join (select policy_id from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = '2020-06-30') claims using(policy_id)
+where date_snapshot = '2020-06-30'
+and claims.policy_id is not null
+and carrier <> 'Canopius'
+and product <> 'HO5'
