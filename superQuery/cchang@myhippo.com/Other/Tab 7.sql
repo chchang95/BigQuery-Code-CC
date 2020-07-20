cb94ad3adc4ed5eee22590e52ebfd7aef7ce7c52
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-20 15:58:04 -0700

    Auto-commit: Mon Jul 20 2020 15:57:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -4,8 +4,8 @@ select policy_id, policy_number
 , property_data_address_zip as zip
 , property_data_address_state as state
 from dw_prod_extracts.ext_policy_snapshots
-left join (select policy_id from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = '2020-06-30') claims using(policy_id)
 where date_snapshot = '2020-06-30'
-and claims.policy_id is not null
 and carrier <> 'Canopius'
+and state not in ('TX', 'CA')
 and product <> 'HO5'
+and calculated_fields_non_cat_risk_class = 'interior_inspection_required'
