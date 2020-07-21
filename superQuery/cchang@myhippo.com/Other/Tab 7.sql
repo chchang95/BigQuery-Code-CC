fffa258f477a4da0c7691a9e7126f038b0145f5b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-21 09:40:01 -0700

    Auto-commit: Tue Jul 21 2020 09:39:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -8,5 +8,5 @@ select state, avg(coverage_a) from dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-07-20'
 and carrier = 'Topa'
 and status = 'active'
-and product = 'HO6'
+and product = 'HO3'
 group by 1
