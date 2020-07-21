975f4b832fff7077e1a928140ed5c7903f26a3ee
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-21 09:28:07 -0700

    Auto-commit: Tue Jul 21 2020 09:28:03 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -1,4 +1,4 @@
-select renewal_term, count(*) from dw_prod_extracts.ext_policy_snapshots 
+select renewal_number, count(*) from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-07-20'
 and carrier = 'Topa'
 group by 1
