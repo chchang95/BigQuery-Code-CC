1278b37f97e84fb7f5bcb6508220d3d804320cdf
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-20 19:44:00 -0700

    Auto-commit: Mon Jul 20 2020 19:43:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -1,3 +1,3 @@
-select coverage_e, count(*) from dw_prod_extracts.ext_policy_snapshots
+select* from dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-07-19'
 and coverage_e > 1000000
