071e0bb2f7e01ed64e74fac6fe959af1e09a66ee
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-21 09:15:51 -0700

    Auto-commit: Tue Jul 21 2020 09:15:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -5,5 +5,5 @@
 -- group by 1
 
 select date_diff(date_policy_expires, date_policy_effective, YEAR) from dw_prod_extracts.ext_policy_snapshots 
-where date_snapshot = '2020-07-30'
+where date_snapshot = '2020-07-20'
 and carrier = 'Topa'
