4bea083e428f7266aa20a98ff3a5bf4cf2dfb908
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-21 09:28:02 -0700

    Auto-commit: Tue Jul 21 2020 09:27:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -1,10 +1,4 @@
--- select coverage_e, count(*) from dw_prod_extracts.ext_policy_snapshots
--- where date_snapshot = '2020-07-19'
--- -- and coverage_e >
--- and carrier <> 'Canopius'
--- group by 1
-
-select date_diff(date_policy_expires, date_policy_effective, MONTH), count(*) from dw_prod_extracts.ext_policy_snapshots 
+select renewal_term, count(*) from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-07-20'
 and carrier = 'Topa'
 group by 1
