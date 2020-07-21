c45e19daad9dcc36c6b2d0d91fb713b2c9f46c2b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-21 09:37:51 -0700

    Auto-commit: Tue Jul 21 2020 09:37:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 7.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 7.sql
@@ -1,4 +1,10 @@
-select renewal_number, count(*) from dw_prod_extracts.ext_policy_snapshots 
+-- select coverage_e, count(*) from dw_prod_extracts.ext_policy_snapshots
+-- where date_snapshot = '2020-07-19'
+-- -- and coverage_e >
+-- and carrier <> 'Canopius'
+-- group by 1
+
+select state, avg(coverage_a) from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-07-20'
 and carrier = 'Topa'
 group by 1
