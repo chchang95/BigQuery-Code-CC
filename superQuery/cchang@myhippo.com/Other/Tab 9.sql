ee6cab5b631a4073c207d41f303a30df3425136c
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-27 10:03:16 -0700

    Auto-commit: Mon Jul 27 2020 10:03:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 9.sql
@@ -1,8 +1,10 @@
 select state, carrier,
 sum(case when renewal_number > 0 then 1 else 0 end) as renewal_count,
-sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count
+sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
+count(policy_id) as total_PIF_count
 from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-06-30'
 and carrier <> 'Canopius'
+and status = 'active'
 group by 1,2
 order by 2,1
