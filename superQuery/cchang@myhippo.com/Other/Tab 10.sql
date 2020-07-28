5b444fd89798c9751725b8b70a246914daaf7a64
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-27 19:49:04 -0700

    Auto-commit: Mon Jul 27 2020 19:48:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 10.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 10.sql
@@ -1,18 +1,19 @@
-select state
--- , carrier
--- , product
--- , case when reinsurance_treaty = 'Spkr19_GAP' and date_report_period_start <= '2019-12-31' then 'Spkr19_GAP'
--- when reinsurance_treaty = 'Spkr19_GAP' and date_report_period_start >= '2020-01-01' then 'Spkr20_Classic'
--- else reinsurance_treaty end as accounting_treaty
--- ,case when reinsurance_treaty = 'Spkr20_Classic' then 'Spkr19_GAP' else reinsurance_treaty end as originating_treaty
--- , substr(policy_number,length(policy_number)-1,2) as term
-, greatest(date_trunc(date_customer_update_made, MONTH), date_trunc(date_update_effective, MONTH), date_report_period_start) as date_accounting
-,sum(earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl
-,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
-from dw_prod_extracts.ext_policy_update_monthly_premiums epud
-left join (select policy_id, policy_number, reinsurance_treaty from dw_prod.dim_policies) dp on epud.policy_id = dp.policy_id
-where date_knowledge = '2020-06-30'
-and carrier <> 'Canopius'
+select distinct policy_id
+, policy_number
+,property_data_address_zip
+,property_data_address_county
+,carrier
+,status
+,state
+,date_policy_effective
+,renewal_number as term
+,product
+,coverage_a
+,eps.quote_rater_version as rater_version
+,eps.written_base + eps.written_total_optionals - eps.written_optionals_equipment_breakdown - eps.written_optionals_service_line as written_x_ebsl
+-- ,quote_premium_base - greatest(coverage_a / 1000 - sum_perils - min_optionals_adjustment ,0) - sum_perils as expense_load
+,0 as expense_load
+from dw_staging_extracts.ext_policy_snapshots eps
+where eps.date_snapshot = '2020-05-31'
+and state = 'TX'
 and product <> 'HO5'
-group by 1,2
-order by 1,2
