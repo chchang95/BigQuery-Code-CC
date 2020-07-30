a149b7a1d60f8179a765e0797a0c26536a517585
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-29 17:12:49 -0700

    Auto-commit: Wed Jul 29 2020 17:12:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Organization Table.sql
+++ superQuery/cchang@myhippo.com/Other/Organization Table.sql
@@ -1,3 +1,4 @@
 select * from dw_prod.dim_organization_mappings_snapshot
 where snapshot_date = '2020-07-13'
-and organization_type = 'Producer'
+-- and organization_type = 'Producer'
+and organization_name = 'home point financial'
