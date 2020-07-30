e854658bb44f70d52b19d6dbe8acc618dc7c6f76
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-07-30 07:49:03 -0700

    Auto-commit: Thu Jul 30 2020 07:48:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Tab 4.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Tab 4.sql
@@ -4,14 +4,14 @@ select eps.policy_id, eps.policy_number
 ,product
 ,org_id as organization_id
 ,case when renewal_number = 0 then "New" else "Renewal" end as tenure
+, case when state = 'tx' and calculated_fields_cat_risk_score = 'referral' then 'referral' 
+        when calculated_fields_non_cat_risk_class is null then 'not_applicable' 
+        else calculated_fields_non_cat_risk_class end as uw_action 
 ,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
 ,sum(earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl
 ,sum(written_exposure) as written_exposure
 ,sum(earned_exposure) as earned_exposure
-, case when state = 'tx' and calculated_fields_cat_risk_score = 'referral' then 'referral' 
-        when calculated_fields_non_cat_risk_class is null then 'not_applicable' 
-        else calculated_fields_non_cat_risk_class end as uw_action 
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
 where date_snapshot = '2020-06-30'
-group by 1,2,3,4,5,6,7
+group by 1,2,3,4,5,6,7,8
