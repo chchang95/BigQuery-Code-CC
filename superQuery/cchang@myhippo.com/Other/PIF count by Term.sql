ecca5a2424f72292aa3c181dfdd9bc131438dfce
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-03 14:12:36 -0700

    Auto-commit: Mon Aug 03 2020 14:05:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
+++ superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
@@ -3,11 +3,9 @@ sum(case when renewal_number > 0 then 1 else 0 end) as renewal_count,
 sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
 count(policy_id) as total_PIF_count,
 sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as total_TIV
-from dw_prod_extracts.ext_policy_snapshots eps
-left join (select date, last_day_of_month from dw_prod.utils_dates where date = date(last_day_of_month)) ud on eps.date_snapshot = date(ud.last_day_of_month)
+from dw_prod_extracts.ext_policy_snapshots 
 where 1=1
-and date_snapshot > '2019-09-01'
-and ud.date is not null
+-- and date_snapshot = '2020-07-30'
 and carrier <> 'Canopius'
 and product <> 'HO5'
 and status = 'active'
