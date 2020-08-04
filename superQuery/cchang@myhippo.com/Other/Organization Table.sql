1ae8b2d798af85963befa75e62f0220ff5e5ee68
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-03 21:16:41 -0700

    Auto-commit: Mon Aug 03 2020 21:16:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Organization Table.sql
+++ superQuery/cchang@myhippo.com/Other/Organization Table.sql
@@ -1,4 +1,9 @@
-select * from dw_prod.dim_organization_mappings_snapshot
-where snapshot_date = '2020-07-13'
--- and organization_type = 'Producer'
-and organization_name = 'home point financial'
+select organization_id, organization_name, root_organization_name, organization_type, organization_type_with_producer_split
+from dw_prod.dim_organization_mappings 
+-- where snapshot_date = '2020-07-13'
+order by organization_id 
+
+-- select * from dw_prod.dim_organization_mappings_snapshot
+-- where snapshot_date = '2020-07-13'
+-- -- and organization_type = 'Producer'
+-- and organization_name = 'homepoint financial'
