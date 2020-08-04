8a2dacc91f35031712b5f797f9f305eb7e68af40
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-03 21:57:58 -0700

    Auto-commit: Mon Aug 03 2020 21:57:48 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/UW Model/Tab 9.sql
@@ -0,0 +1,34 @@
+select cast(id as string)   as policy_number
+             , least(cast(json_extract_scalar(transaction,'$.calculated_fields.age_of_home') as numeric),
+                     77)                                                               as age_of_home
+             , case
+                   when coalesce(json_extract_scalar(transaction,'$.property_data.guard'), 'false') = 'false' then 'No'
+                   else 'Yes' end                                                      as Guard
+             , json_extract_scalar(transaction,'$.calculated_fields.age_of_insured')               as age_of_insured
+             , json_extract_scalar(transaction,'$.calculated_fields.three_years_claims')           as Three_Years_Claims
+             , json_extract_scalar(transaction,'$.property_data.number_of_stories')                as Number_Of_Stories
+             , JSON_EXTRACT(data,'$.promotional_score.report.insurance_score') as pg_insurance_score
+             , case
+                   when coalesce(json_extract_scalar(transaction,'$.property_data.hoa_membership'), 'false') = 'false' then 'No'
+                   else 'Yes' end                                                      as Hoa_Membership
+             , cast('lead' as string)                                                                  as quote_type
+        from postgres_public.leads a
+                 left join (select lead_id, bound, status
+                            from postgres_public.policies
+                            where policy_number like '%-00'
+                              and bound = true) b
+                           on cast(a.id as string) = cast(b.lead_id as string)
+        where 1 = 1
+--       and bound = 'true'
+--       and status not in ('pending_active', 'pending_bind')
+--       and effective_date::date <= '2020-04-31'::date
+          and cast(initial_quote_date as date) >= '2019-10-20'
+--       and state = 'tx'
+          and carrier <> 'canopius'
+          and product not in ('ho5')
+          and json_extract_scalar(transaction,'$.quote.premium.total') is not null
+          and state is not null
+          and initial_quote_date is not null
+          and json_extract_scalar(transaction,'$.effective_date') is not null
+          and id = '2785651'
+        --   LIMIT 50000
