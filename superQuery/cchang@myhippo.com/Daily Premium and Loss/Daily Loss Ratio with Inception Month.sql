b2817fdf1c493609683c6cd93278d05273be6aaf
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-04 18:09:39 -0700

    Auto-commit: Tue Aug 04 2020 18:09:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio with Inception Month.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio with Inception Month.sql
@@ -13,16 +13,9 @@ policy_id_2 as policy_id
 ,zip
 ,calculated_fields_non_cat_risk_score
 ,sum(total_incurred) as total_incurred
-,sum(case when CAT = 'N' then total_incurred else 0 end) as non_cat_incurred
-,sum(case when CAT = 'Y' then total_incurred else 0 end) as cat_incurred
-,sum(case when claim_closed_no_total_payment is false then 1 else 0 end) as total_claim_count_x_cnp
-,sum(case when claim_closed_no_total_payment is false and CAT = 'N' then 1 else 0 end) as non_cat_claim_count_x_cnp
-,sum(case when claim_closed_no_total_payment is false and CAT = 'Y' then 1 else 0 end) as cat_claim_count_x_cnp
-,sum(case when CAT = 'Y' then 0 when total_incurred >= 100000 then 100000 else total_incurred end) as capped_non_cat_incurred
-,sum(case when CAT = 'Y' then 0 when total_incurred >= 100000 then total_incurred - 100000 else 0 end) as excess_non_cat_incurred
-from claims_supp
-where ebsl = 'N'
+from ext_claims_inception_to_date
+where 
+1=1
 and state = 'CA'
 and carrier = 'Topa'
 group by 1,2,3,4,5,6,7,8,9,10,11,12)
-select sum(total_incurred)
