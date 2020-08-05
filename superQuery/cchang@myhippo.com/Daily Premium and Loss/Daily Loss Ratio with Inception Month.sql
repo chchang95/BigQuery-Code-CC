8e942e1a3a12d162ee59a2f70c8761615a989d80
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-04 18:06:01 -0700

    Auto-commit: Tue Aug 04 2020 18:05:44 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio with Inception Month.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio with Inception Month.sql
@@ -119,10 +119,10 @@ policy_id, state, product, carrier, accounting_treaty, accident_month, tenure, p
 -- , round(sum(total_incurred) / sum(earned_prem_x_ebsl),3) as total_incurred
 from combined
 where 1=1
-and accident_month >= '2019-09-01'
+and accident_month >= '2019-01-01'
 and earned_prem_x_ebsl <> 0
 and state = 'CA'
 group by 1,2,3,4,5,6,7,8,9,10,11,12
 order by 1,2,3
 )
-select * from summary
+select sum(total_incurred) from summary
