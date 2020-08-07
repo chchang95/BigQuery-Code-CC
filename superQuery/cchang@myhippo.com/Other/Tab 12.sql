301f9c900ded7259457cb62206911efec01355bf
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-06 17:09:08 -0700

    Auto-commit: Thu Aug 06 2020 17:08:54 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -0,0 +1,24 @@
+-- SELECT
+--     mon.month_knowledge,
+--     mon.carrier,
+--     mon.state,
+--     mon.product,
+--     month_of_loss,
+--     maturity,
+--     case when mon.peril = 'wind' or mon.peril = 'hail' then 'Y'
+--       when is_catastrophe is true then 'Y'
+--       else 'N' end as CAT
+--     ,reinsurance_treaty
+--     ,case when reinsurance_treaty = 'Spkr20_Classic' then 'Spkr19_GAP' else reinsurance_treaty end as original_treaty
+--     ,sum(case when claim_closed_no_total_payment is true then 0 else 1 end) as claim_count_x_cnp
+--     ,sum(total_incurred_inception_to_date) as total_incurred_cumulative
+--     ,sum(total_incurred_delta_this_month) as total_incurred_incremental
+--     ,sum(case when total_incurred_inception_to_date >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_catastrophe is true) then 100000 else total_incurred_inception_to_date end) as capped_NC_total_incurred_cumulative
+--     ,sum(case when total_incurred_inception_to_date >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_catastrophe is true) then total_incurred_inception_to_date - 100000 else 0 end) as excess_NC_total_incurred_cumulative
+--   FROM
+--     dw_prod_extracts.ext_claim_monthly mon
+--     left join (select claim_number, reinsurance_treaty from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = @as_of) USING(claim_number)
+--   where is_ebsl is false
+--   group by 1,2,3,4,5,6,7,8,9
+  
+ select * from dw_prod_extracts.ext_all_claims_combined
