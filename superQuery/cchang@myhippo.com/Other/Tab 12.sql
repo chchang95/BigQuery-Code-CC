a22ddf188e3dc6e7f71d45e8860cd1c78fb2efe9
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-07 18:29:19 -0700

    Auto-commit: Fri Aug 07 2020 18:29:10 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 12.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -66,7 +66,7 @@ and carrier = 'Topa'
 -- and date_knowledge = '2020-07-31'
 and extract(MONTH from date_knowledge) in (3,6,9,12)
 -- and peril not in ('equipment_breakdown', 'service_line')
-group by 1,2,3
+group by 3,1,2
 
 -- Paid loss
 -- Incurred loss
