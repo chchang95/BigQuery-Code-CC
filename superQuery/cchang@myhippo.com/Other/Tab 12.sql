c7f3fbf6fa0422f6a9e98ad12dca5ef48a60d2d6
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-07 17:26:31 -0700

    Auto-commit: Fri Aug 07 2020 17:26:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 12.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -41,7 +41,7 @@ from dw_prod_extracts.ext_all_claims_combined
 where 1=1
 and carrier <> 'Canopius'
 and date_knowledge = '2020-07-31'
-and date_report_period = '2020-07-31'
+and date_report_period_end = '2020-07-31'
 group by 1,2
 
 -- Paid loss
