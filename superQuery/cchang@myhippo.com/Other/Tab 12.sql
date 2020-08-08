c956eeb03fe3f3e8584470ca9b5e2b2411336b80
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-07 17:36:10 -0700

    Auto-commit: Fri Aug 07 2020 17:36:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 12.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -1,4 +1,4 @@
-select distinct peril_group from dw_prod_extracts.ext_all_claims_combined
+select distinct peril_group, peril from dw_prod_extracts.ext_all_claims_combined
 where 1=1
 and tbl_source = 'topa_tpa_claims'
 -- -- and claim_status = 'TBD'
