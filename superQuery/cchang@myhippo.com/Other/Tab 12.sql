34e7f69358510cebfdbef206d6f4c150c507b835
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-07 17:05:04 -0700

    Auto-commit: Fri Aug 07 2020 17:04:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 12.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -1,4 +1,4 @@
-select distinct claim_status from dw_prod_extracts.ext_all_claims_combined
--- where tbl_source = 'topa_tpa_claims'
--- --  group by 1
-order by 1
+select * from dw_prod_extracts.ext_all_claims_combined
+where 1=1
+-- and tbl_source = 'topa_tpa_claims'
+and claim_status = 'TBD'
