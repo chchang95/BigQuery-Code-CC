cab494c375d5594f6c24ec29c02d65339eb1e302
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-07 17:37:01 -0700

    Auto-commit: Fri Aug 07 2020 17:36:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 12.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 12.sql
@@ -1,6 +1,6 @@
-select distinct peril_group, peril from dw_prod_extracts.ext_all_claims_combined
+select tbl_source, max(date_knowledge) from dw_prod_extracts.ext_all_claims_combined
 where 1=1
-and tbl_source = 'topa_tpa_claims'
--- -- and claim_status = 'TBD'
---  group by 1
+-- and tbl_source = 'topa_tpa_claims'
+-- and claim_status = 'TBD'
+ group by 1
 order by 1
