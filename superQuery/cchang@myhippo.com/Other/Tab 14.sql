647ff3cf4f1071e63731b5d91b9ff6cdd1b277db
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-11 15:26:59 -0700

    Auto-commit: Tue Aug 11 2020 15:26:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 14.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 14.sql
@@ -1,44 +1,43 @@
 select eps.policy_id
 ,status
-,dp.policy_number
+,policy_number
 ,date_policy_effective as effective_date
 ,date_policy_expires as expiration_date
 ,property_data_address_street as street
 ,property_data_address_city as city
 ,property_data_address_zip as zip
 ,property_data_address_county as county
+,org_id
 ,state 
 ,product 
 ,written_base + written_total_optionals + written_policy_fee as written_total
 ,written_base
 ,written_policy_fee
 ,written_total_optionals
-,written_optionals_equipment_breakdown
-,written_optionals_service_line
+,written_optionals_equipment_breakdown + written_optionals_service_line as written_EBSL
 ,written_exposure
 ,earned_base + earned_total_optionals + earned_policy_fee as earned_total
 ,earned_base
 ,earned_policy_fee
 ,earned_total_optionals
-,earned_optionals_equipment_breakdown
-,earned_optionals_service_line
+,earned_optionals_equipment_breakdown + earned_optionals_service_line as earned_EBSL
 ,earned_exposure
-,property_data_roof_type as Roof_Type
-,property_data_roof_shape as Roof_Shape
-,property_data_year_built as Year_Built
-,property_data_residence_type as Residence_Type
-,property_data_square_footage as Square_Footage
-,property_data_foundation_type as Foundation_Type
-,property_data_year_roof_built as Year_Roof_Built
-,property_data_protection_class as Protection_Class
-,property_data_construction_type as Construction_Type
-,property_data_number_of_stories as Number_Of_Stories
-,property_data_hail_resistant_roof as Hail_Resistant_Roof
-,property_data_number_of_family_units as Number_Of_Family_Units
-,prefilled_basement_finished_percent as Basement_Finished_Percent
-,property_data_other_wind_loss_prevention as Other_Wind_Loss_Prevention
-,calculated_fields_age_of_roof as Age_Of_Roof
-,calculated_fields_wind_exclusion as Wind_Exclusion
+-- ,property_data_roof_type as Roof_Type
+-- ,property_data_roof_shape as Roof_Shape
+-- ,property_data_year_built as Year_Built
+-- ,property_data_residence_type as Residence_Type
+-- ,property_data_square_footage as Square_Footage
+-- ,property_data_foundation_type as Foundation_Type
+-- ,property_data_year_roof_built as Year_Roof_Built
+-- ,property_data_protection_class as Protection_Class
+-- ,property_data_construction_type as Construction_Type
+-- ,property_data_number_of_stories as Number_Of_Stories
+-- ,property_data_hail_resistant_roof as Hail_Resistant_Roof
+-- ,property_data_number_of_family_units as Number_Of_Family_Units
+-- ,prefilled_basement_finished_percent as Basement_Finished_Percent
+-- ,property_data_other_wind_loss_prevention as Other_Wind_Loss_Prevention
+-- ,calculated_fields_age_of_roof as Age_Of_Roof
+-- ,calculated_fields_wind_exclusion as Wind_Exclusion
 ,coverage_deductible as Deductible
 ,coverage_hurricane_deductible as Hurricane_Deductible
 ,coverage_wind_deductible as Wind_Deductible
@@ -47,7 +46,7 @@ select eps.policy_id
 ,coverage_c as cov_c
 ,coverage_d as cov_d
 from dw_prod_extracts.ext_policy_snapshots eps
-left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(policy_id)
+left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
 where date_snapshot = '2020-07-31'
 and carrier <> 'Canopius'
