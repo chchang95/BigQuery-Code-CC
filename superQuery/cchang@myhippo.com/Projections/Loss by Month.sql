6882b5999765a0ed7dcf01bd1b3447735806c98f
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-20 19:10:35 -0700

    Auto-commit: Thu Aug 20 2020 19:10:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Projections/Loss by Month.sql
+++ superQuery/cchang@myhippo.com/Projections/Loss by Month.sql
@@ -1,1 +1,25 @@
-  select *, case when date_close is null then 'Y' else 'N' end from dw_prod_extracts.ext_claim_monthly where claim_number = 'CAZ-0430278-01-01'
+SELECT
+    mon.month_knowledge,
+    mon.carrier,
+    mon.state,
+    mon.product,
+    month_of_loss,
+    maturity,
+    case when mon.peril = 'wind' or mon.peril = 'hail' then 'Y'
+      when is_catastrophe is true then 'Y'
+      else 'N' end as CAT
+    ,reinsurance_treaty
+    ,case when reinsurance_treaty = 'Spkr20_Classic' then 'Spkr19_GAP' else reinsurance_treaty end as original_treaty
+    ,sum(case when claim_closed_no_total_payment is true then 0 else 1 end) as claim_count_x_cnp
+    ,sum(case when cast(date_close as numeric) = 0 or date_close is null then 0 when claim_closed_no_total_payment is true then 0 else 1 end) as paid_claim_count_x_cnp
+    ,sum(expense_calculated_incurred_inception_to_date) as ALAE_cumulative
+    ,sum(loss_calculated_incurred_inception_to_date) as Indemnity_cumulative
+    ,sum(total_incurred_inception_to_date) as total_incurred_cumulative
+    ,sum(total_incurred_delta_this_month) as total_incurred_incremental
+    ,sum(case when total_incurred_inception_to_date >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_catastrophe is true) then 100000 else total_incurred_inception_to_date end) as capped_NC_total_incurred_cumulative
+    ,sum(case when total_incurred_inception_to_date >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_catastrophe is true) then total_incurred_inception_to_date - 100000 else 0 end) as excess_NC_total_incurred_cumulative
+  FROM
+    dw_prod_extracts.ext_claim_monthly mon
+    left join (select claim_number, reinsurance_treaty from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = @as_of) USING(claim_number)
+  where is_ebsl is false
+  group by 1,2,3,4,5,6,7,8,9
