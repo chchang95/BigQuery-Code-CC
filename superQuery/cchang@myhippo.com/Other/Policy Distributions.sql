2674cb6b9a740e83ca826515d9d3f92fd82538aa
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-26 11:57:16 -0700

    Auto-commit: Wed Aug 26 2020 11:57:08 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
+++ superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
@@ -1,6 +1,20 @@
-select state, avg(cast(calculated_fields_coverages_tiv as numeric)) from dw_prod_extracts.ext_policy_snapshots 
+-- select coverage_e, count(*) from dw_prod_extracts.ext_policy_snapshots
+-- where date_snapshot = '2020-07-19'
+-- -- and coverage_e >
+-- and carrier <> 'Canopius'
+-- group by 1
+
+select state, ROUND(avg(cast(calculated_fields_coverages_tiv as numeric)),0) from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-07-31'
 -- and state = 'CA'
 and status = 'active'
 -- and product = 'HO3'
 group by 1
+order by 1
+
+-- select distinct state, property_data_address_zip from dw_prod_extracts.ext_policy_snapshots 
+-- where date_snapshot = '2020-07-20'
+-- and carrier = 'Topa'
+-- and status = 'active'
+-- and product = 'HO3'
+-- group by 1
