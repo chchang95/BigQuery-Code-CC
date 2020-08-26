5d479296354ea518d68890d0fafa865daf371e30
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-26 11:56:51 -0700

    Auto-commit: Wed Aug 26 2020 11:56:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
+++ superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
@@ -1,4 +1,4 @@
-select state, avg(calculated_fields_tiv) from dw_prod_extracts.ext_policy_snapshots 
+select state, avg(cast(calculated_fields_coverages_tiv as numeric)) from dw_prod_extracts.ext_policy_snapshots 
 where date_snapshot = '2020-07-31'
 -- and state = 'CA'
 and status = 'active'
