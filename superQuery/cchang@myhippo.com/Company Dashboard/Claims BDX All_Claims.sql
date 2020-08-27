09e53d3ef251c28d8c2f27cc1110ef2150b23c23
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-27 15:16:25 -0700

    Auto-commit: Thu Aug 27 2020 15:16:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX All_Claims.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX All_Claims.sql
@@ -1,3 +1,2 @@
-
-  select tbl_source, max(date_knowledge) from dw_staging_extracts.ext_all_claims_combined
+  select tbl_source, max(date_report_period_end) from dw_staging_extracts.ext_all_claims_combined
   group by 1
