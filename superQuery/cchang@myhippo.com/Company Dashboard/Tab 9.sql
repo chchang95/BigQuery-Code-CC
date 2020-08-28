ef9c1ef06d620e6e70e69fe84150d187efa81d70
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-28 13:57:11 -0700

    Auto-commit: Fri Aug 28 2020 13:57:05 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Tab 9.sql
@@ -1,5 +1,5 @@
 select reinsurance_treaty, sum(Total_Incurred_Loss_and_ALAE) as total_incurred, sum(earned) as EP, sum(Total_Incurred_Loss_and_ALAE) /sum(earned) as total_LR
 from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_combined
--- where date_bordereau = '2020-06-30'
+where date_bordereau = '2020-06-30'
 group by 1
 order by 1
