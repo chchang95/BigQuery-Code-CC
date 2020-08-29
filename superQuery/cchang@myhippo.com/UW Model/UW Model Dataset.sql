154efe688e49e9421595b0a2d91ff50823741a11
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-28 19:15:46 -0700

    Auto-commit: Fri Aug 28 2020 19:15:30 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/UW Model/UW Model Dataset.sql
@@ -0,0 +1,73 @@
+with claims_supp as (
+SELECT DISTINCT
+    mon.*
+    ,dp.org_id
+      , case when peril = 'equipment_breakdown' or peril = 'service_line' then 'Y'
+      when is_EBSL = true then 'Y'
+      else 'N' end as EBSL
+      , case when peril = 'wind' or peril = 'hail' then 'Y'
+      when cat_code is not null then 'Y'
+      else 'N' end as CAT
+  FROM dw_staging_extracts.ext_all_claims_combined mon
+  left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
+  WHERE date_report_period_end = '2020-07-31'
+  and carrier <> 'Canopius'
+  )
+, claims as (
+select
+policy_id
+,date_trunc(date_of_loss, MONTH) as accident_month
+,sum(total_incurred) as total_incurred
+,sum(case when CAT = 'N' then total_incurred else 0 end) as non_cat_incurred
+,sum(case when CAT = 'Y' then 0 when total_incurred >= 100000 then 100000 else total_incurred end) as capped_non_cat_incurred
+,sum(case when CAT = 'Y' then 0 when total_incurred >= 100000 then total_incurred - 100000 else 0 end) as excess_non_cat_incurred
+,sum(case when CAT = 'Y' then total_incurred else 0 end) as cat_incurred
+,sum(case when claim_closed_no_total_payment is false then 1 else 0 end) as reported_total_claim_count_x_cnp
+,sum(case when claim_closed_no_total_payment is false and CAT = 'N' then 1 else 0 end) as reported_non_cat_claim_count_x_cnp
+,sum(case when claim_closed_no_total_payment is false and CAT = 'Y' then 1 else 0 end) as reported_cat_claim_count_x_cnp
+,sum(case when claim_closed_no_total_payment is false and date_closed is not null then 1 else 0 end) as closed_total_claim_count_x_cnp
+,sum(case when claim_closed_no_total_payment is false and date_closed is not null and CAT = 'N' then 1 else 0 end) as closed_non_cat_claim_count_x_cnp
+,sum(case when claim_closed_no_total_payment is false and date_closed is not null and CAT = 'Y' then 1 else 0 end) as closed_cat_claim_count_x_cnp
+from claims_supp
+where ebsl = 'N'
+group by 1,2
+) 
+select 
+eps.policy_id, eps.policy_number
+, date_trunc(date_policy_effective, MONTH) as policy_inception_month
+,date_policy_effective
+-- ,date_
+,carrier
+,state
+,product
+,status 
+,org_id as organization_id
+,property_data_address_zip as zip
+,property_data_address_county as county
+,case when renewal_number = 0 then "New" else "Renewal" end as tenure
+, case when state = 'tx' and calculated_fields_cat_risk_score = 'referral' then 'referral' 
+        when calculated_fields_non_cat_risk_class is null then 'not_applicable' 
+        else calculated_fields_non_cat_risk_class end as uw_action 
+, calculated_fields_non_cat_risk_score as non_cat_risk_score
+,written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line as written_prem_x_ebsl
+,earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line as earned_prem_x_ebsl
+,written_exposure
+,earned_exposure
+,coalesce(total_incurred,0) as total_incurred
+,coalesce(non_cat_incurred,0) as non_cat_incurred
+,coalesce(capped_non_cat_incurred,0) as capped_non_cat_incurred
+,coalesce(excess_non_cat_incurred,0) as excess_non_cat_incurred
+,coalesce(cat_incurred,0) as cat_incurred
+,coalesce(reported_total_claim_count_x_cnp,0) as reported_total_claim_count_x_cnp
+,coalesce(reported_non_cat_claim_count_x_cnp,0) as reported_non_cat_claim_count_x_cnp
+,coalesce(reported_cat_claim_count_x_cnp,0) as reported_cat_claim_count_x_cnp
+,coalesce(closed_total_claim_count_x_cnp,0) as closed_total_claim_count_x_cnp
+,coalesce(closed_non_cat_claim_count_x_cnp,0) as closed_non_cat_claim_count_x_cnp
+,coalesce(closed_cat_claim_count_x_cnp,0) as closed_cat_claim_count_x_cnp
+from dw_prod_extracts.ext_policy_snapshots eps
+left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
+left join claims c on eps.policy_id = c.policy_id
+where date_snapshot = '2020-07-31'
+and carrier <> 'Canopius'
+and product <> 'HO5'
+and state = 'CA'
