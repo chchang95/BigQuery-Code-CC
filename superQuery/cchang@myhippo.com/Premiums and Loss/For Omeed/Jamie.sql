2ba4e908895737a9e35d29a22d2c74c2c1b91c0b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-31 16:00:27 -0700

    Auto-commit: Mon Aug 31 2020 16:00:16 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Premiums and Loss/For Omeed/Jamie.sql
@@ -0,0 +1,27 @@
+SELECT
+    date_report_period_end,
+    mon.carrier,
+    mon.property_data_address_state,
+    mon.product,
+    accident_month,
+    maturity,
+    case when mon.peril = 'wind' or mon.peril = 'hail' then 'Y'
+      when is_cat is true then 'Y'
+      else 'N' end as CAT
+    ,reinsurance_treaty
+    ,peril
+    ,case when reinsurance_treaty = 'Spkr20_Classic' then 'Spkr19_GAP' else reinsurance_treaty end as original_treaty
+    ,sum(case when claim_closed_no_total_payment is true then 0 else 1 end) as claim_count_x_cnp
+    ,sum(case when date_closed is null then 0 when claim_closed_no_total_payment is true then 0 else 1 end) as paid_claim_count_x_cnp
+    ,sum(expense_incurred) as ALAE_cumulative
+    ,sum(loss_incurred) as Indemnity_cumulative
+    ,sum(recoveries) as Recoveries_cumulative
+    ,sum(total_incurred) as total_incurred_cumulative
+    ,sum(recoverable_depreciation) as recoverable_depreciation_cumulative
+    ,sum(case when total_incurred >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_cat is true) then 100000 else total_incurred end) as capped_NC_total_incurred_cumulative
+    ,sum(case when total_incurred >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_cat is true) then total_incurred - 100000 else 0 end) as excess_NC_total_incurred_cumulative
+  FROM
+    dw_prod_extracts.ext_all_claims_combined mon
+    -- left join (select claim_number, reinsurance_treaty from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = @as_of) USING(claim_number)
+  where is_ebsl is false
+  group by 1,2,3,4,5,6,7,8,9,10
