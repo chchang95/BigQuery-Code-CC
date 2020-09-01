eaa39e15f6bfbda1a7c6a29422d24a5e8d574eb1
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-31 22:21:24 -0700

    Auto-commit: Mon Aug 31 2020 22:21:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Premiums and Loss/For Omeed/Jamie.sql
+++ superQuery/cchang@myhippo.com/Premiums and Loss/For Omeed/Jamie.sql
@@ -26,7 +26,7 @@ SELECT
     ,sum(case when total_incurred >= 100000 and not (mon.peril = 'wind' or mon.peril = 'hail' or is_cat is true) then total_incurred - 100000 else 0 end) as excess_NC_total_incurred_cumulative
   FROM
     dw_prod_extracts.ext_all_claims_combined mon
-    left join (select distinct policy_number, note from dw_staging.chin_partner_handled_claims) phc on phc.claim_number = mon.claim_number 
+    left join (select distinct claim_number, note from dw_staging.chin_partner_handled_claims) phc on phc.claim_number = mon.claim_number 
     -- left join (select claim_number, reinsurance_treaty from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = @as_of) USING(claim_number)
   where is_ebsl is false
   group by 1,2,3,4,5,6,7,8,9,10,11
