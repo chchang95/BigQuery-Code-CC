20c53c0e273a69c5b9a2a4dd1aa1b2ee51ad3c44
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-08-31 23:06:18 -0700

    Auto-commit: Mon Aug 31 2020 23:06:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Tab 10.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Tab 10.sql
@@ -7,8 +7,17 @@ select *
 from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_premium
 )
 , policies as (
-select *
-from dw_prod_extracts.ext_policy_snapshots
+select eps.policy_id, eps.policy_number
+, carrier
+, state
+, product
+, date_trunc(date_policy_effective, MONTH) as effective_month
+, case when renewal_number > 0 then 'Renewal' else 'New' end as tenure 
+, channel
+, org_id as organization_id
+from dw_prod_extracts.ext_policy_snapshots eps
+    left join (select policy_id, policy_number, channel, attributed_organization_id
+    , case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
 where date_snapshot = '2020-07-31'
 )
 , combined AS (
@@ -26,78 +35,73 @@ where date_snapshot = '2020-07-31'
   AND premium.date_knowledge = loss.date_bordereau
   AND premium.date_report_period_start = loss.date_accident_month_begin
 )
-select date_accident_month_begin, 
-SUM(coalesce(total_incurred_loss_and_alae,0)) as total_incurred,
-sum(coalesce(incurred_loss_cat,0)) as total_cat,
-sum(coalesce(incurred_loss_noncat,0)) as total_noncat
-from combined
-where date_bordereau = '2020-07-31'
-and reinsurance_treaty not in ('Spkr17_MRDP_EBSL','Topa_EBSL','Spkr19_HSBOld','Spkr19_HSBNew','Canopius')
-group by 1
-order by 1
+-- select date_accident_month_begin, 
+-- SUM(coalesce(total_incurred_loss_and_alae,0)) as total_incurred,
+-- sum(coalesce(incurred_loss_cat,0)) as total_cat,
+-- sum(coalesce(incurred_loss_noncat,0)) as total_noncat
+-- from combined
+-- where date_bordereau = '2020-07-31'
+-- and reinsurance_treaty not in ('Spkr17_MRDP_EBSL','Topa_EBSL','Spkr19_HSBOld','Spkr19_HSBNew','Canopius')
+-- group by 1
+-- order by 1
 
--- , enhanced AS (
---   SELECT combined.*,
---          EXTRACT(year FROM date_accident_month_begin) AS accident_year,
---          policies.carrier,
---          policies.organization_id,
---          policies.channel,
---          CASE
---           WHEN policies.attributed_organization_id IS NULL AND channel = 'Online' THEN 0
---           WHEN policies.attributed_organization_id IS NULL AND channel = 'Agent' THEN -99
---           ELSE organization_id
---          END AS fixed_attributed_organization_id,
---          policies.state,
---          policies.product,
---          CASE
---           WHEN policies.renewal_number = 0 THEN 'new'
---           ELSE 'renewal'
---          END AS tenure
---   FROM combined
---   INNER JOIN policies
---   USING(policy_id)
--- )
--- SELECT reinsurance_treaty,
---       state,
---       carrier,
---       product,
---       date_bordereau,
---       accident_year,
---       date_accident_month_begin,
---       date_accident_month_end,
---       organization_id,
---       channel,
---       fixed_attributed_organization_id,
---       tenure,
---       COALESCE(CAST(SUM(earned) AS FLOAT64),0) AS earned,
---       COALESCE(CAST(SUM(written) AS FLOAT64),0) AS written,
---       COALESCE(CAST(SUM(earned_exposure) AS FLOAT64),0) AS earned_exposure,
---       COALESCE(CAST(SUM(earned_tiv) AS FLOAT64),0) AS earned_tiv,
---       COALESCE(SUM(total_Claim_Count),0) AS total_Claim_Count,
---       COALESCE(SUM(Total_Paid_Indemnity),0) AS Total_Paid_Indemnity,
---       COALESCE(SUM(Total_Case_Reserve_Indemnity),0) AS Total_Case_Reserve_Indemnity,
---       COALESCE(SUM(Total_Paid_ALAE),0) AS Total_Paid_ALAE,
---       COALESCE(SUM(Total_Case_Reserve_ALAE),0) AS Total_Case_Reserve_ALAE,
---       COALESCE(SUM(Salvage_Subro),0) AS Salvage_Subro,
---       COALESCE(SUM(Total_incurred_Indemnity),0) AS Total_Incurred_Indemnity,
---       COALESCE(SUM(Total_Incurred_ALAE),0) AS Total_Incurred_ALAE,
---       COALESCE(SUM(Total_Incurred_Loss_and_ALAE),0) AS Total_Incurred_Loss_and_ALAE,
---       COALESCE(SUM(Claim_Count_CAT),0) AS Claim_Count_CAT,
---     --   COALESCE(SUM(total_cat_incurred_loss_and_alae),0) AS total_cat_incurred_loss_and_alae,
---       COALESCE(SUM(Incurred_Loss_CAT),0) AS Total_Incurred_Loss_CAT,
---       COALESCE(SUM(Claim_Count_NonCAT),0) AS Claim_Count_NonCAT,
---       COALESCE(SUM(Capped_NonCAT_loss_and_ALAE),0) AS Capped_NonCAT_loss_and_ALAE,
---       COALESCE(SUM(Excess_Loss_NonCAT),0) AS Excess_Loss_NonCAT,
---       COALESCE(SUM(Incurred_Loss_NonCAT), 0) AS Total_Incurred_Loss_NonCAT,
---       COALESCE(SUM(Excess_Count_NonCAT),0) AS Excess_Count_NonCAT,
---     --   {% for peril_type in peril_type_list %}
---     --     COALESCE(SUM(Claim_Count_{{ peril_type }}_CAT),0) AS Claim_Count_{{ peril_type }}_CAT,
---     --     COALESCE(SUM(Incurred_Loss_{{ peril_type }}_CAT),0) AS Incurred_Loss_{{ peril_type }}_CAT,
---     --     COALESCE(SUM(Claim_Count_{{ peril_type }}_NonCAT),0) AS Claim_Count_{{ peril_type }}_NonCAT,
---     --     COALESCE(SUM(Capped_Loss_{{ peril_type }}_NonCAT),0) AS Capped_Loss_{{ peril_type }}_NonCAT,
---     --     COALESCE(SUM(Excess_Loss_{{ peril_type }}_NonCAT),0) AS Excess_Loss_{{ peril_type }}_NonCAT,
---     --     COALESCE(SUM(Incurred_Loss_{{ peril_type }}_NonCAT),0) AS Incurred_Loss_{{ peril_type }}_NonCAT,
---     --     COALESCE(SUM(Excess_Count_{{ peril_type }}_NonCAT),0) AS Excess_Count_{{ peril_type }}_NonCAT,
---     --   {% endfor %}
--- FROM enhanced
--- GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+, enhanced AS (
+  SELECT combined.*,
+         EXTRACT(year FROM date_accident_month_begin) AS accident_year,
+         policies.carrier,
+         policies.organization_id,
+         policies.channel,
+         policies.state,
+         policies.product,
+         policies.tenure,
+         policies.effective_month
+  FROM combined
+  INNER JOIN policies
+  USING(policy_id)
+)
+, final as (
+SELECT reinsurance_treaty,
+      state,
+      carrier,
+      product,
+      date_bordereau,
+      accident_year,
+      date_accident_month_begin,
+      date_accident_month_end,
+      organization_id,
+      channel,
+      tenure,
+      effective_month,
+      COALESCE(CAST(SUM(earned) AS FLOAT64),0) AS earned,
+      COALESCE(CAST(SUM(written) AS FLOAT64),0) AS written,
+      COALESCE(CAST(SUM(earned_exposure) AS FLOAT64),0) AS earned_exposure,
+      COALESCE(CAST(SUM(earned_tiv) AS FLOAT64),0) AS earned_tiv,
+      COALESCE(SUM(total_Claim_Count),0) AS total_Claim_Count,
+      COALESCE(SUM(Total_Paid_Indemnity),0) AS Total_Paid_Indemnity,
+      COALESCE(SUM(Total_Case_Reserve_Indemnity),0) AS Total_Case_Reserve_Indemnity,
+      COALESCE(SUM(Total_Paid_ALAE),0) AS Total_Paid_ALAE,
+      COALESCE(SUM(Total_Case_Reserve_ALAE),0) AS Total_Case_Reserve_ALAE,
+      COALESCE(SUM(Salvage_Subro),0) AS Salvage_Subro,
+      COALESCE(SUM(Total_incurred_Indemnity),0) AS Total_Incurred_Indemnity,
+      COALESCE(SUM(Total_Incurred_ALAE),0) AS Total_Incurred_ALAE,
+      COALESCE(SUM(Total_Incurred_Loss_and_ALAE),0) AS Total_Incurred_Loss_and_ALAE,
+      COALESCE(SUM(Claim_Count_CAT),0) AS Claim_Count_CAT,
+    --   COALESCE(SUM(total_cat_incurred_loss_and_alae),0) AS total_cat_incurred_loss_and_alae,
+      COALESCE(SUM(Incurred_Loss_CAT),0) AS Total_Incurred_Loss_CAT,
+      COALESCE(SUM(Claim_Count_NonCAT),0) AS Claim_Count_NonCAT,
+      COALESCE(SUM(Capped_NonCAT_loss_and_ALAE),0) AS Capped_NonCAT_loss_and_ALAE,
+      COALESCE(SUM(Excess_Loss_NonCAT),0) AS Excess_Loss_NonCAT,
+      COALESCE(SUM(Incurred_Loss_NonCAT), 0) AS Total_Incurred_Loss_NonCAT,
+      COALESCE(SUM(Excess_Count_NonCAT),0) AS Excess_Count_NonCAT,
+    --   {% for peril_type in peril_type_list %}
+    --     COALESCE(SUM(Claim_Count_{{ peril_type }}_CAT),0) AS Claim_Count_{{ peril_type }}_CAT,
+    --     COALESCE(SUM(Incurred_Loss_{{ peril_type }}_CAT),0) AS Incurred_Loss_{{ peril_type }}_CAT,
+    --     COALESCE(SUM(Claim_Count_{{ peril_type }}_NonCAT),0) AS Claim_Count_{{ peril_type }}_NonCAT,
+    --     COALESCE(SUM(Capped_Loss_{{ peril_type }}_NonCAT),0) AS Capped_Loss_{{ peril_type }}_NonCAT,
+    --     COALESCE(SUM(Excess_Loss_{{ peril_type }}_NonCAT),0) AS Excess_Loss_{{ peril_type }}_NonCAT,
+    --     COALESCE(SUM(Incurred_Loss_{{ peril_type }}_NonCAT),0) AS Incurred_Loss_{{ peril_type }}_NonCAT,
+    --     COALESCE(SUM(Excess_Count_{{ peril_type }}_NonCAT),0) AS Excess_Count_{{ peril_type }}_NonCAT,
+    --   {% endfor %}
+FROM enhanced
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+)
