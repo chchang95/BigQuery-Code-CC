3abec2627d8a59c33613232594f6c5538610dfd1
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-02 12:25:07 -0700

    Auto-commit: Wed Sep 02 2020 12:24:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
@@ -1,10 +1,10 @@
-select state, sum(written), sum(earned)
-from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_premium
-left join dw_prod.dim_policies using(policy_id)
-where date_knowledge = '2020-08-31'
--- and reinsurance_treaty = 'Topa'
-and carrier <> 'Canopius'
-group by 1
+-- select state, sum(written), sum(earned)
+-- from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_premium
+-- left join dw_prod.dim_policies using(policy_id)
+-- where date_knowledge = '2020-08-31'
+-- -- and reinsurance_treaty = 'Topa'
+-- and carrier <> 'Canopius'
+-- group by 1
 
 
 -- select sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
@@ -12,3 +12,7 @@ group by 1
 -- from dw_prod_extracts.ext_policy_monthly_premiums epud
 -- where date_knowledge = '2020-08-31'
 -- and reinsurance_treaty_accounting = 'Topa'
+
+
+select * from dw_prod_extracts.ext_actuarial_monthly_loss_ratios_loss
+where policy_id = 109830
