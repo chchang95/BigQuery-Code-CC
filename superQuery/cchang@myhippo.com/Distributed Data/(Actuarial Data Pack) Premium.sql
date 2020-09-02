40441d0897484a4814c6bd319c5f2416e208a092
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-01 20:46:16 -0700

    Auto-commit: Tue Sep 01 2020 20:45:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
@@ -92,7 +92,7 @@ GROUP BY 1, 2, 3, 4, 5, 6, 7
 )
 , aggregated as (
 select state,
-sum(Written_Premium_x_ebsl_inc_pol_fee)
+sum(Written_Premium_x_ebsl_inc_pol_fee),
 SUM(coalesce(Total_Incurred_Loss_and_ALAE,0)) as total_incurred,
 -- sum(coalesce(Incurred_Loss_CAT,0)) as total_cat,
 -- sum(coalesce(Incurred_Loss_NonCAT,0)) as total_noncat
