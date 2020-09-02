1b06fb7d203b53a6e7e9f12cbe4546372311c62c
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-02 12:10:44 -0700

    Auto-commit: Wed Sep 02 2020 12:10:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
@@ -52,7 +52,7 @@ where date_snapshot = '2020-08-31'
 , final as (
 SELECT 
 -- date_bordereau
-state
+policy_id
 ,carrier
 ,product
 ,accident_year
@@ -94,13 +94,14 @@ and reinsurance_treaty not in ('Spkr17_MRDP_EBSL','Topa_EBSL','Spkr19_HSBOld','S
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 )
 , aggregated as (
-select state,
+select policy_number,
 sum(Written_Premium_Including_Policy_Fee),
 sum(Written_Policy_Fee),
 SUM(coalesce(Total_Incurred_Loss_and_ALAE,0)) as total_incurred,
 -- sum(coalesce(Incurred_Loss_CAT,0)) as total_cat,
 -- sum(coalesce(Incurred_Loss_NonCAT,0)) as total_noncat
-from final
+from final 
+left join dw_prod.dim_policies using(policy_id)
 group by 1
 order by 1
 )
