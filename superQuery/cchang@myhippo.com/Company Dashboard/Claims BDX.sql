11ccb83cc8742e7d885e92db6b3901696bfd6643
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-02 09:16:12 -0700

    Auto-commit: Wed Sep 02 2020 09:15:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX.sql
@@ -49,7 +49,7 @@ SELECT DISTINCT
   FROM dw_prod_extracts.ext_claims_inception_to_date mon
 --   left join (select claim_id, loss_description, damage_description from dw_prod.fct_claims) fc using (claim_id)
   left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
-  WHERE date_knowledge = '2020-08-31'
+  WHERE date_knowledge = '2020-09-01'
   and carrier <> 'Canopius'
   )
   select 'Hippo' as ClaimsHandler
@@ -85,7 +85,7 @@ SELECT DISTINCT
   from x
   where 1=1
 --   and ebsl = 'N'
-  and carrier = 'Topa'
+--   and carrier = 'Topa'
   
 --   select * from dw_prod_extracts.ext_claims_inception_to_date mon
 --   where claim_number = 'HCA-1029622-00-01'
