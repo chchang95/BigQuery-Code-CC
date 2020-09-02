5350dca1d78eba062ad36089930275810a307ebd
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-01 20:23:49 -0700

    Auto-commit: Tue Sep 01 2020 20:23:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
@@ -1,8 +1,9 @@
 select sum(written), sum(earned)
 from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_premium
 where date_knowledge = '2020-08-31'
-and carrier <> 'Canopius'
-and state = 'CA'
+-- and carrier <> 'Canopius'
+-- and state = 'CA'
+-- written = 
 
 -- select sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
 -- ,sum(earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl
