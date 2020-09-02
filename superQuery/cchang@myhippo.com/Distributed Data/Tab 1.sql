ff998129be6aaf050ba9b048ea117e8c4312b28b
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-01 20:35:03 -0700

    Auto-commit: Tue Sep 01 2020 20:34:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 1.sql
@@ -1,7 +1,9 @@
-select sum(written), sum(earned)
+select state, sum(written), sum(earned)
 from dw_staging_extracts.ext_actuarial_monthly_loss_ratios_premium
+left join dw_prod.dim_policies using(policy_id)
 where date_knowledge = '2020-08-31'
-and reinsurance_treaty = 'Topa'
+-- and reinsurance_treaty = 'Topa'
+and carrier <> 'Canopius'
 
 
 -- select sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
