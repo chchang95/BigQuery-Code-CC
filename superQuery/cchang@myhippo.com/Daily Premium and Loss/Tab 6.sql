7ec09df4447e5b5f83100f0f76843f05c88df458
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-03 15:11:13 -0700

    Auto-commit: Thu Sep 03 2020 15:11:05 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Tab 6.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Tab 6.sql
@@ -4,4 +4,4 @@ select policy_id, policy_number
 from dw_prod_extracts.ext_policy_snapshots
 where date_policy_effective >= '2020-05-01'
 and date_snapshot = '2020-08-31'
-where calculated_fields_non_cat_risk_class is null
+and calculated_fields_non_cat_risk_class is null
