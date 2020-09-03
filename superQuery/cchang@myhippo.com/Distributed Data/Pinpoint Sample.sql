55c6e17e6ec7b5d4ddf1f583e765b04eb0651045
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-02 23:07:13 -0700

    Auto-commit: Wed Sep 02 2020 23:07:00 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Pinpoint Sample.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Pinpoint Sample.sql
@@ -1,3 +1,22 @@
+with policy_claims as (
+# 13338
+select distinct policy_id 
+, personal_information_first_name as first_name
+, personal_information_last_name as last_name
+, personal_information_email as email
+, personal_information_phone_number as phone_number
+, property_data_address_street as street
+, property_data_address_city as city
+, property_data_address_zip as zip
+, property_data_address_state as state
+, 'c' as type
+from dw_prod_extracts.ext_policy_snapshots
+left join (select policy_id from dw_prod_extracts.ext_actuarial_monthly_loss_ratios_loss where date_bordereau = '2020-08-31') claims using(policy_id)
+where date_snapshot = '2020-08-31'
+and claims.policy_id is not null
+and carrier <> 'Canopius'
+and product <> 'HO5'
+), policies as (
 select policy_id
 , personal_information_first_name as first_name
 , personal_information_last_name as last_name
@@ -12,4 +31,11 @@ from dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-08-31'
 and carrier <> 'Canopius'
 and product <> 'HO5'
-and date_policy_effective >= '2020-01-01'
+and date_policy_effective >= '2020-03-31'
+)
+, combined as (
+select * from policy_claims
+union all
+select * from policies
+)
+select distinct * from combined
