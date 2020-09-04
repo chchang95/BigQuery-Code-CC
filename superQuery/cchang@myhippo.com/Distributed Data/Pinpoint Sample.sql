70fc6724515bb56c9514262c4764a9446270b0b3
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-04 13:00:17 -0700

    Auto-commit: Fri Sep 04 2020 13:00:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Pinpoint Sample.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Pinpoint Sample.sql
@@ -1,4 +1,4 @@
-select distinct policy_id 
+select policy_id
 , date_policy_effective
 , personal_information_first_name as first_name
 , personal_information_last_name as last_name
@@ -8,13 +8,16 @@ select distinct policy_id
 , property_data_address_city as city
 , property_data_address_zip as zip
 , property_data_address_state as state
+-- , 'r' as type
 from dw_prod_extracts.ext_policy_snapshots eps
-    left join (select policy_id from dw_prod_extracts.ext_actuarial_monthly_loss_ratios_loss where date_bordereau = '2020-08-31') claims using(policy_id)
     left join (select id, policy_number, json_extract_scalar(policy_info,'$.property_data.purchase_date') as purchase_date
     from postgres_public.policies 
     where bound = true ) pd on pd.id = eps.policy_id
 where date_snapshot = '2020-08-31'
-and claims.policy_id is not null
-and (pd.purchase_date = 'next_3_months' or pd.purchase_date = 'last_12_months')
 and carrier <> 'Canopius'
 and product <> 'HO5'
+and status = 'active'
+and earned_exposure > 0
+and pd.purchase_date = 'next_3_months'
+and date_policy_effective >= '2020-06-01'
+-- and date_policy_effective >= '2020-01-01'
