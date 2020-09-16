507b87cfa568a4a4aa7df9fef5f98607b6a6455d
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-16 11:51:42 -0700

    Auto-commit: Wed Sep 16 2020 11:51:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 17.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 17.sql
@@ -3,6 +3,7 @@ sum(written_base + written_total_optionals)/ COUNT(*)
 from dw_prod_extracts.ext_policy_snapshots
 where status = 'active'
 and date_snapshot = '2020-08-31'
+and date_policy_effective >= '2020-07-31'
 and product <> 'HO5'
 -- and product = 'HO5'
 group by 1
