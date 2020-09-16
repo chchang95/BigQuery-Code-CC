05c14d29f0b88245cd41f1579852cf536b2d78de
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-16 12:17:47 -0700

    Auto-commit: Wed Sep 16 2020 12:17:41 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 17.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 17.sql
@@ -1,10 +1,11 @@
-select state, 
+select state, channel, 
 sum(written_base + written_total_optionals)/ sum(written_exposure)
 from dw_prod_extracts.ext_policy_snapshots
+left join dw_prod.dim_policies using(policy_id)
 where status = 'active'
 and date_snapshot = '2020-08-31'
 and date_policy_effective >= '2020-07-31'
 and product <> 'HO5'
 -- and product = 'HO5'
-group by 1
+group by 1,2
 order by 1
