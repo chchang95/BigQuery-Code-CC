58385d9932ca52cfcf8b4603d4fb804f1dbfc20d
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-15 19:19:22 -0700

    Auto-commit: Tue Sep 15 2020 19:19:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 17.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 17.sql
@@ -1,7 +1,7 @@
 select state, sum(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line)/ COUNT(*)
 from dw_prod_extracts.ext_policy_snapshots
 where status = 'active'
-and date_snapshot = '2020-07-31'
+and date_snapshot = '2020-08-31'
 and product = 'HO5'
 group by 1
 order by 1
