324752b4eca5a2177895e9900577d699e464a12a
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-15 19:16:55 -0700

    Auto-commit: Tue Sep 15 2020 19:16:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Tab 17.sql
+++ superQuery/cchang@myhippo.com/Other/Tab 17.sql
@@ -2,5 +2,6 @@ select state, sum(written_base + written_total_optionals - written_optionals_equ
 from dw_prod_extracts.ext_policy_snapshots
 where status = 'active'
 and date_snapshot = '2020-07-31'
+and product = 'HO5'
 group by 1
 order by 1
