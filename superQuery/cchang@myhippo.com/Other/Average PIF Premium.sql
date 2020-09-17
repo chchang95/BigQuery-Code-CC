3dd726626645e8b8ce5ca98ce4d9c977b420a974
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-17 09:47:41 -0700

    Auto-commit: Thu Sep 17 2020 09:47:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
+++ superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
@@ -1,4 +1,5 @@
-select eps.state, channel, 
+select eps.state, case when eps.renewal_number = 0 then 'new' else 'renewal' end as tenure,
+-- channel, 
 sum(written_base + written_total_optionals)/ sum(written_exposure)
 from dw_prod_extracts.ext_policy_snapshots eps
 left join dw_prod.dim_policies using(policy_id)
@@ -6,7 +7,7 @@ where status = 'active'
 and date_snapshot = '2020-08-31'
 and date_policy_effective >= '2020-07-31'
 -- and eps.product <> 'HO5'
-and eps.renewal_number > 0
+-- and eps.renewal_number > 0
 -- and channel
 and eps.product = 'HO5'
 group by 1,2
