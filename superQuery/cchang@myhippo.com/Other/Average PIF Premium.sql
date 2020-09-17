2346834858a038ed99706e9082858c4ce108e011
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-16 21:46:24 -0700

    Auto-commit: Wed Sep 16 2020 21:46:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
+++ superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
@@ -7,7 +7,7 @@ and date_snapshot = '2020-08-31'
 and date_policy_effective >= '2020-07-31'
 -- and eps.product <> 'HO5'
 and eps.renewal_number > 0
-and channel
+-- and channel
 and eps.product = 'HO5'
 group by 1,2
 order by 1
