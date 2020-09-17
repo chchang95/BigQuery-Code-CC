2584e0298c29e80e101748ac72f27f279dd0c5b7
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-17 15:30:11 -0700

    Auto-commit: Thu Sep 17 2020 15:29:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
@@ -92,7 +92,7 @@ group by 1,2,3,4
 -- order by 1,2
 )
 , summary as (
-select accounting_treaty
+select carrier
 , sum(written_prem_x_ebsl) as written_prem, sum(earned_prem_x_ebsl) as earned_prem
 , sum(capped_non_cat_incurred) as capped_non_cat_incurred
 , sum(excess_non_cat_incurred) as excess_non_cat_incurred
