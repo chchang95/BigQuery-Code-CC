d6be277944b5919ae21a98defc5e3967a248ffec
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-16 21:41:17 -0700

    Auto-commit: Wed Sep 16 2020 21:41:06 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
@@ -0,0 +1,13 @@
+select eps.state, channel, 
+sum(written_base + written_total_optionals)/ sum(written_exposure)
+from dw_prod_extracts.ext_policy_snapshots eps
+left join dw_prod.dim_policies using(policy_id)
+where status = 'active'
+and date_snapshot = '2020-08-31'
+and date_policy_effective >= '2020-07-31'
+-- and eps.product <> 'HO5'
+and eps.renewal_number > 0
+and channel
+and product = 'HO5'
+group by 1,2
+order by 1
