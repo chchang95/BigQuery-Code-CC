a3649ba2e04390bb4960d2634864a4c81afaafd7
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-18 08:56:52 -0700

    Auto-commit: Fri Sep 18 2020 08:56:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
+++ superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
@@ -9,6 +9,7 @@ from dw_prod_extracts.ext_policy_snapshots eps
 left join dw_prod.dim_policies using(policy_id)
 where status = 'active'
 and date_snapshot between '2019-05-31' and '2020-05-31'
+and extract(day from DATEADD(day, 1, date_snapshot)) = 1
 -- and date_policy_effective >= '2020-07-31'
 -- and eps.product <> 'HO5'
 -- and eps.renewal_number > 0
