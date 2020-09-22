9c3fd863f785f808bfff29ec093f21f8c94ddc81
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-21 18:05:12 -0700

    Auto-commit: Mon Sep 21 2020 18:05:00 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Other/W.sql
@@ -0,0 +1,24 @@
+select 
+-- eps.state,
+-- date_snapshot
+date_trunc(date_policy_effective, MONTH) as eff_month
+-- case when eps.renewal_number = 0 then 'new' else 'renewal' end as tenure,
+,property_data_address_county
+-- channel, 
+,sum(written_base + written_total_optionals)/ sum(written_exposure) as avg_WP
+,count(*) as count
+from dw_prod_extracts.ext_policy_snapshots eps
+left join dw_prod.dim_policies using(policy_id)
+where status = 'active'
+-- and date_snapshot between '2019-05-31' and '2020-05-31'
+and date_snapshot = '2020-08-31'
+and extract(day from DATE_ADD(date_snapshot, interval 1 day)) = 1
+-- and date_policy_effective >= '2020-07-31'
+-- and eps.product <> 'HO5'
+-- and eps.renewal_number > 0
+-- and channel
+and eps.product = 'ho3'
+-- and eps.carrier = 'topa'
+and eps.state = 'tx'
+group by 1,2
+order by 1
