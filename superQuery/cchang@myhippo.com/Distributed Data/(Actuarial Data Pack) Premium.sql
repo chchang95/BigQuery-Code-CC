61ec8e3701a4eeace505400255821ba71b9387fe
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-22 20:30:36 -0700

    Auto-commit: Tue Sep 22 2020 20:30:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/(Actuarial Data Pack) Premium.sql
@@ -92,7 +92,7 @@ policy_id
       COALESCE(SUM(Excess_Count_NonCAT),0) AS NonCat_Claim_Count_Above_100k,
 FROM enhanced
 where date_bordereau = '2020-08-31'
-and reinsurance_treaty not in ('Spkr17_MRDP_EBSL','Topa_EBSL','Spkr19_HSBOld','Spkr19_HSBNew','Canopius','Canopius_EBSL', 'Topa20_PostAugust_EBSL')
+and reinsurance_treaty not in ('spkr17_mrdp_ebsl','topa_ebsl','spkr19_hsbold','spkr19_hsbnew','canopius','canopius_ebsl', 'topa20_postaugust_ebsl')
 -- and carrier <> 'Canopius'
 GROUP BY 1
 --, 2, 3, 4, 5, 6, 7, 8, 9
