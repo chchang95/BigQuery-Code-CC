8e054ae96b9daab153bc012053160aaba0caf929
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-23 22:25:09 -0700

    Auto-commit: Wed Sep 23 2020 22:25:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
+++ superQuery/cchang@myhippo.com/Premiums and Loss/Surplus Tax.sql
@@ -4,7 +4,7 @@ select
         , greatest(date_trunc(date_customer_update_made, MONTH), date_trunc(date_update_effective, MONTH), date_report_period_start) as date_accounting_start
         ,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
 from dw_prod_extracts.ext_policy_update_monthly_premiums epud
-where carrier = 'Canopius'
+where carrier = 'canopius'
 and date_knowledge = '2020-08-31'
 group by 1,2,3
 order by 1,2,3
