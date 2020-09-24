b87d507832a56b6ca4e1674d24cba84ac06ccc6c
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-24 12:15:28 -0700

    Auto-commit: Thu Sep 24 2020 12:15:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
@@ -7,15 +7,18 @@ select state
 ,org_id as organization_id
 ,date_trunc(date_effective, MONTH) as policy_effective_month
 ,case when renewal_number = 0 then "New" else "Renewal" end as tenure
+,date_trunc(ph.date_first_effective, MONTH) as original_effective_month
 ,sum(written_base + written_total_optionals  - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl_x_pol_fee
 ,sum(earned_base + earned_total_optionals - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl_x_pol_fee
 ,sum(written_exposure) as written_exposure
 ,sum(earned_exposure) as earned_exposure
 from dw_prod_extracts.ext_today_knowledge_policy_monthly_premiums mon
+left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = mon.policy_id
+left join dw_prod.dim_policy_histories ph on dep.policy_group_number = ph.policy_history_number
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
 where date_knowledge = '2020-09-23'
-and carrier <> 'Canopius'
-group by 1,2,3,4,5,6,7,8
+and carrier <> 'canopius'
+group by 1,2,3,4,5,6,7,8,9
 )
 , claims_supp as (
 select * 
@@ -27,8 +30,10 @@ select *
 from dw_prod_extracts.ext_claims_inception_to_date cd
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on cd.policy_id = dp.policy_id
 left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-08-13') eps on eps.policy_id = cd.policy_id
+left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = cd.policy_id
+left join dw_prod.dim_policy_histories ph on dep.policy_group_number = ph.policy_history_number
   WHERE date_knowledge = '2020-09-23'
-  and carrier <> 'Canopius'
+  and carrier <> 'canopius'
 )
 , claims as (
 select
@@ -40,6 +45,7 @@ state
 ,org_id as organization_id
 ,date_trunc(date_effective, MONTH) as policy_effective_month
 ,case when renewal_number = 0 then "New" else "Renewal" end as tenure
+,date_trunc(date_first_effective, MONTH) as original_effective_month
 ,sum(total_incurred) as total_incurred
 ,sum(case when CAT = 'N' then total_incurred else 0 end) as non_cat_incurred
 ,sum(case when CAT = 'Y' then total_incurred else 0 end) as cat_incurred
@@ -50,7 +56,7 @@ state
 ,sum(case when CAT = 'Y' then 0 when total_incurred >= 100000 then total_incurred - 100000 else 0 end) as excess_non_cat_incurred
 from claims_supp
 where ebsl = 'N'
-group by 1,2,3,4,5,6,7,8
+group by 1,2,3,4,5,6,7,8,9
 ) 
 , combined as (
 select p.*
@@ -72,6 +78,7 @@ and p.accounting_treaty = c.reinsurance_treaty
 and p.organization_id = c.organization_id
 and p.policy_effective_month = c.policy_effective_month
 and p.tenure = c.tenure
+and p.original_effective_month = c.original_effective_month
 )
 , aggregated as (
 select state, accounting_treaty, accident_month, tenure, policy_effective_month
@@ -96,7 +103,7 @@ group by 1,2,3,4,5
 -- order by 1,2
 )
 , summary as (
-select accounting_treaty
+select original_effective_month
 , sum(written_prem_x_ebsl_x_pol_fee) as written_prem_x_ebsl_pol_fee, sum(earned_prem_x_ebsl_x_pol_fee) as earned_prem_x_ebsl_x_pol_fee
 , sum(capped_non_cat_incurred) as capped_non_cat_incurred
 , sum(excess_non_cat_incurred) as excess_non_cat_incurred
@@ -110,7 +117,7 @@ select accounting_treaty
 , round(sum(cat_claim_count) / sum(earned_exposure),3) as cat_frequency
 from combined
 where 1=1
-and accident_month = '2020-08-01'
+and accident_month = '2020-98-01'
 -- and accounting_treaty = 'topa20_post_august'
 -- and policy_effective_month = '2020-09-01'
 group by 1
