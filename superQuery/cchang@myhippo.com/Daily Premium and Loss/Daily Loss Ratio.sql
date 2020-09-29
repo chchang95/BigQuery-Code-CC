4633cface5876457c3e6f2c96ac2727508cfc408
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-29 14:04:27 -0700

    Auto-commit: Tue Sep 29 2020 14:04:10 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
@@ -16,7 +16,7 @@ from dw_staging_extracts.ext_today_knowledge_policy_monthly_premiums mon
 left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = mon.policy_id
 left join dw_prod.dim_policy_histories ph on dep.policy_group_number = ph.policy_history_number
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
-where date_knowledge = '2020-09-27'
+where date_knowledge = '2020-09-28'
 and carrier <> 'canopius'
 group by 1,2,3,4,5,6,7,8,9
 )
@@ -29,10 +29,10 @@ select *
         else 'N' end as CAT
 from dw_prod_extracts.ext_claims_inception_to_date cd
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on cd.policy_id = dp.policy_id
-left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-09-27') eps on eps.policy_id = cd.policy_id
+left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-09-28') eps on eps.policy_id = cd.policy_id
 left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = cd.policy_id
 left join dw_prod.dim_policy_histories ph on dep.policy_group_number = ph.policy_history_number
-  WHERE date_knowledge = '2020-09-27'
+  WHERE date_knowledge = '2020-09-28'
   and carrier <> 'canopius'
 )
 , claims as (
