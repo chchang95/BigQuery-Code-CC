8589843644379140b0bda6cd107b902b3ad62f79
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-09-30 13:20:11 -0700

    Auto-commit: Wed Sep 30 2020 13:19:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/UW Model/Quote Counts 2.0.sql
+++ superQuery/cchang@myhippo.com/UW Model/Quote Counts 2.0.sql
@@ -88,4 +88,4 @@ SELECT
       and q.state = 'TX'
       and q.product <> 'ho5'
       and q.carrier <> 'canopius'
-      group by 1,2,3,4,5,6,7
+      group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
