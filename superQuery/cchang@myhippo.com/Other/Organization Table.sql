3667f73cc1121442e5212317361b5b768715edda
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-11 18:36:52 -0700

    Auto-commit: Sun Oct 11 2020 18:36:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Organization Table.sql
+++ superQuery/cchang@myhippo.com/Other/Organization Table.sql
@@ -1,7 +1,7 @@
 select organization_id, organization_name, root_organization_name, organization_type, organization_type_with_producer_split
 from dw_prod.dim_organization_mappings 
 -- where snapshot_date = '2020-07-13'
-order by organization_id 
+order by organization_id desc
 
 -- select * from dw_prod.dim_organization_mappings_snapshot
 -- where snapshot_date = '2020-07-13'
