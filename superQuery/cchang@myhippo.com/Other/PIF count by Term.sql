45dc1b2e19f807a4f1bb7ae3aeed3d224f169a94
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-11 18:51:16 -0700

    Auto-commit: Sun Oct 11 2020 18:51:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
+++ superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
@@ -1,11 +1,16 @@
 select state, carrier, date_snapshot,
 sum(case when renewal_number > 0 then 1 else 0 end) as renewal_count,
 sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
-count(policy_id) as total_PIF_count,
+count(eps.policy_id) as total_PIF_count,
 sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as total_TIV
-from dw_prod_extracts.ext_policy_snapshots 
+from dw_prod_extracts.ext_policy_snapshots eps
+left join (select policy_id, policy_number, channel, attributed_organization_id
+    , case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
+left join (select organization_id, organization_name, root_organization_name, from dw_prod.dim_organization_mappings) org_table on f.organization_id = org_table.organization_id
+left join (select date, last_day_of_month from dw_prod.utils_dates where date = date(last_day_of_month)) ud on eps.date_snapshot = date(ud.last_day_of_month)
 where 1=1
--- and date_snapshot = '2020-07-30'
+and date_snapshot > '2019-09-01'
+and ud.date is not null
 and carrier <> 'Canopius'
 and product <> 'HO5'
 and status = 'active'
