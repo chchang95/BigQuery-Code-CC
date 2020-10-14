3609f1801de923ecd6cfc72f1e17680855fd13f8
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-13 21:48:07 -0700

    Auto-commit: Tue Oct 13 2020 21:48:04 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
+++ superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
@@ -4,12 +4,15 @@
 -- and carrier <> 'Canopius'
 -- group by 1
 
-select state, ROUND(avg(cast(calculated_fields_coverages_tiv as numeric)),0), avg(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line) from dw_prod_extracts.ext_policy_snapshots 
-where date_snapshot = '2020-07-31'
+select payment_frequency, payment_method
+, ROUND(avg(cast(calculated_fields_coverages_tiv as numeric)),0), avg(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line)
+,count(policy_number)
+from dw_prod_extracts.ext_policy_snapshots 
+where date_snapshot = '2020-09-30'
 -- and state = 'CA'
 and status = 'active'
 -- and product = 'HO3'
-group by 1
+group by 1,2
 order by 1
 
 -- select distinct state, property_data_address_zip from dw_prod_extracts.ext_policy_snapshots 
