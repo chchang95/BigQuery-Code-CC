609a20ae5d741d9af59d90e3d305c9ab3dc01348
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-13 21:48:21 -0700

    Auto-commit: Tue Oct 13 2020 21:48:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
+++ superQuery/cchang@myhippo.com/Other/Policy Distributions.sql
@@ -4,7 +4,7 @@
 -- and carrier <> 'Canopius'
 -- group by 1
 
-select payment_frequency, payment_method
+select payment_frequency, 
 , ROUND(avg(cast(calculated_fields_coverages_tiv as numeric)),0), avg(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line)
 ,count(policy_number)
 from dw_prod_extracts.ext_policy_snapshots 
