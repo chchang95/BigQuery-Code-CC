3dca2b1ec77b82334630730bef5620ff7bb3be9f
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-18 19:38:46 -0700

    Auto-commit: Sun Oct 18 2020 19:38:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
+++ superQuery/cchang@myhippo.com/Other/Average PIF Premium.sql
@@ -10,7 +10,7 @@ date_trunc(date_policy_effective, MONTH) as eff_month
 from dw_prod_extracts.ext_policy_snapshots eps
 left join dw_prod.dim_policies using(policy_id)
 where status = 'active'
-and date_snapshot between '2019-05-31' and '2020-09-30'
+and date_snapshot between '2018-01-31' and '2020-09-30'
 -- and date_snapshot = '2020-08-31'
 and extract(day from DATE_ADD(date_snapshot, interval 1 day)) = 1
 -- and date_policy_effective >= '2020-07-31'
