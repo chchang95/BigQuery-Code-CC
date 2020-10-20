c6b0e1ede9e4f4ee4026a56e2dc54d22cadd64e0
chchang95 <67664723+chchang95@users.noreply.github.com>
2020-10-19 21:26:34 -0700

    Auto-commit: Mon Oct 19 2020 21:26:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
+++ superQuery/cchang@myhippo.com/Other/PIF count by Term.sql
@@ -1,4 +1,6 @@
-select state, carrier, date_snapshot, org_id, organization_name, root_organization_name,
+select state,
+-- , carrier, date_snapshot
+-- , org_id, organization_name, root_organization_name,
 -- sum(case when renewal_number > 0 then 1 else 0 end) as renewal_count,
 -- sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
 count(eps.policy_id) as total_PIF_count,
@@ -9,9 +11,9 @@ left join (select policy_id, policy_number, channel, attributed_organization_id
 left join (select organization_id, organization_name, root_organization_name, from dw_prod.dim_organization_mappings) org_table on dp.org_id = org_table.organization_id
 left join (select date, last_day_of_month from dw_prod.utils_dates where date = date(last_day_of_month)) ud on eps.date_snapshot = date(ud.last_day_of_month)
 where 1=1
-and date_snapshot > '2019-09-01'
+and date_snapshot = '2019-09-30'
 and ud.date is not null
-and carrier <> 'canopius'
+and carrier = 'spinnaker'
 and product <> 'ho5'
 and status = 'active'
 group by 1,2,3,4,5,6
