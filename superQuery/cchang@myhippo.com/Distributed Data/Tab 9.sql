6e938c75e65eb92622406c931ccd80aa95dfe117
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-21 14:28:14 -0700

    Auto-commit: Wed Oct 21 2020 14:28:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
@@ -9,7 +9,7 @@
 -- group by 1,2
 -- order by 1,2
 
-select policy_id, property_data_address_county, property_data_fireline_score,
+select policy_number, property_data_address_county, property_data_fireline_score,
 sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as TIV
 FROM dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-08-31'
