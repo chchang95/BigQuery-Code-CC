6a57629c0edc7b85b75a83c01adf671cdbaac64a
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-21 13:58:32 -0700

    Auto-commit: Wed Oct 21 2020 13:58:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
@@ -18,6 +18,6 @@ and product = 'ho3'
 and carrier = 'topa'
 and state = 'ca'
 and status = 'active'
-and property_data_fireline_score*1 > 1
+and cast(property_data_fireline_score as numeric) > 1
 group by 1,2,3
 order by 1,2
