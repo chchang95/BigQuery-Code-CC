f6aa79c9914547681be8807d1ae1f88416f474a9
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-21 14:29:24 -0700

    Auto-commit: Wed Oct 21 2020 14:29:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
@@ -14,10 +14,10 @@ sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + c
 FROM dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-08-31'
 and product <> 'ho5'
-and product = 'ho3'
+-- and product = 'ho3'
 and carrier = 'topa'
 and state = 'ca'
 and status = 'active'
-and cast(property_data_fireline_score as numeric) > 1
+-- and cast(property_data_fireline_score as numeric) > 1
 group by 1,2,3
 order by 1,2
