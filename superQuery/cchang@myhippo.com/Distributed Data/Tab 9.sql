8215da8f4b631af163fd35f4ddcb1fe753cc6327
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-21 16:27:47 -0700

    Auto-commit: Wed Oct 21 2020 16:27:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Distributed Data/Tab 9.sql
@@ -9,7 +9,7 @@
 -- group by 1,2
 -- order by 1,2
 
-select policy_number, property_data_address_county, property_data_fireline_score,
+select policy_number, property_data_address_county, property_data_fireline_score, date_policy_effective, property_data_address_zip,
 sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as TIV
 FROM dw_prod_extracts.ext_policy_snapshots
 where date_snapshot = '2020-08-31'
@@ -19,5 +19,5 @@ and carrier = 'topa'
 and state = 'ca'
 and status = 'active'
 and cast(property_data_fireline_score as numeric) > 1
-group by 1,2,3
+group by 1,2,3,4,5
 order by 1,2
