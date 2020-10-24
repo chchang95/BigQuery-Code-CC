1a918f9b899a9c7bd83c2a92abbeb1e0388c1501
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-23 17:09:35 -0700

    Auto-commit: Fri Oct 23 2020 17:09:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Capping Testing.sql
+++ superQuery/cchang@myhippo.com/Other/Capping Testing.sql
@@ -1,3 +1,3 @@
 select *
-from unnest((select renewals from s3.az_nv_policy_rate_capping_data))
+from unnest(cast((select renewals from s3.az_nv_policy_rate_capping_data) as array))
 where renewals is not null
