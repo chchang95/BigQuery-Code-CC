57483bfbd673e15f38668801306f5f5b0db421fe
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-23 17:39:57 -0700

    Auto-commit: Fri Oct 23 2020 17:39:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Capping Testing.sql
+++ superQuery/cchang@myhippo.com/Other/Capping Testing.sql
@@ -1,11 +1,12 @@
 with x as (
 select target_policy_number,JSON_EXTRACT(renewals,'$[0]') as first_renewal, JSON_EXTRACT(renewals,'$[1]') as second_renewal, existing_rate_cap_amount
 from s3.az_nv_policy_rate_capping_data
-where renewals is not null)
+where renewals is not null
+)
 select target_policy_number,
 
-json_extract(existing_rate_cap_amount, '$.current_term') as current_term_cap
-json_extract(existing_rate_cap_amount, '$.next_term') as next_term_cap
+json_extract(existing_rate_cap_amount, '$.current_term') as current_term_cap,
+json_extract(existing_rate_cap_amount, '$.next_term') as next_term_cap,
 
 json_extract(first_renewal, '$.rateCapPercentage') as first_renewal_rate_cap,  
 
@@ -28,6 +29,5 @@ json_extract(second_renewal, '$.newRaterVersionQuote.premium.base') as second_re
 json_extract(second_renewal, '$.newRaterVersionQuote.premium.optionals') as second_renewal_new_prem_optionals,
 json_extract(second_renewal, '$.newRaterVersionQuote.premium.fees') as second_renewal_new_prem_fees,
 json_extract(second_renewal, '$.newRaterVersionQuote.premium.total') as second_renewal_new_prem_total,
-
-
 from x
+where first_renewal is not null
