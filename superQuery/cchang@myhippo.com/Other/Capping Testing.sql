7c7a0e7802257582756545f635710ba02a72d56a
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-23 17:30:26 -0700

    Auto-commit: Fri Oct 23 2020 17:30:22 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Capping Testing.sql
+++ superQuery/cchang@myhippo.com/Other/Capping Testing.sql
@@ -1,3 +1,6 @@
-select target_policy_number,JSON_EXTRACT(renewals,'$[0]')
+with x as (
+select target_policy_number,JSON_EXTRACT(renewals,'$[0]') as first_renewal, JSON_EXTRACT(renewals,'$[1]') as second_renewal
 from s3.az_nv_policy_rate_capping_data
-where renewals is not null
+where renewals is not null)
+select target_policy_number, json_extract(first_renewal, '$.oldRaterVersionQuote.premium') as first_renewal_prem 
+from x
