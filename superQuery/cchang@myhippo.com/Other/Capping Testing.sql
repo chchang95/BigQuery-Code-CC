d3a5c455e1a1e948b49262297f3671d78fa5e7b0
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-23 17:33:09 -0700

    Auto-commit: Fri Oct 23 2020 17:33:03 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/Capping Testing.sql
+++ superQuery/cchang@myhippo.com/Other/Capping Testing.sql
@@ -3,8 +3,24 @@ select target_policy_number,JSON_EXTRACT(renewals,'$[0]') as first_renewal, JSON
 from s3.az_nv_policy_rate_capping_data
 where renewals is not null)
 select target_policy_number,
-json_extract(first_renewal, '$.oldRaterVersionQuote.premium.base') as first_renewal_prem_base,  
-json_extract(first_renewal, '$.oldRaterVersionQuote.premium.optionals') as first_renewal_prem_optionals,
-json_extract(first_renewal, '$.oldRaterVersionQuote.premium.fees') as first_renewal_prem_fees,
-json_extract(first_renewal, '$.oldRaterVersionQuote.premium.total') as first_renewal_prem_total,
+
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.base') as first_renewal_old_prem_base,  
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.optionals') as first_renewal_old_prem_optionals,
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.fees') as first_renewal_old_prem_fees,
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.total') as first_renewal_old_prem_total,
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.base') as first_renewal_new_prem_base,  
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.optionals') as first_renewal_new_prem_optionals,
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.fees') as first_renewal_new_prem_fees,
+json_extract(first_renewal, '$.oldRaterVersionQuote.premium.total') as first_renewal_new_prem_total,
+
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.base') as second_renewal_old_prem_base,  
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.optionals') as second_renewal_old_prem_optionals,
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.fees') as second_renewal_old_prem_fees,
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.total') as second_renewal_old_prem_total,
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.base') as second_renewal_new_prem_base,  
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.optionals') as second_renewal_new_prem_optionals,
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.fees') as second_renewal_new_prem_fees,
+json_extract(second_renewal, '$.oldRaterVersionQuote.premium.total') as second_renewal_new_prem_total,
+
+
 from x
