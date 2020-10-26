a12cea101972d3ce8bcee06749e7070c408428f0
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-25 22:28:07 -0700

    Auto-commit: Sun Oct 25 2020 22:28:02 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/AIG Distribution.sql
+++ superQuery/cchang@myhippo.com/Other/AIG Distribution.sql
@@ -5,9 +5,9 @@ date_snapshot,
 date_trunc(date_policy_effective, MONTH) as eff_month,
 case when eps.renewal_number = 0 then 'new' else 'renewal' end as tenure,
 channel, 
-eps.coverage_a,
-eps.calculated_fields_age_of_home,
-eps.insurance_score
+cast(eps.coverage_a as numeric) - mod(cast(eps.coverage_a as numeric),10000),
+cast(eps.calculated_fields_age_of_home as numeric) - mod(cast(eps.calculated_fields_age_of_home as numeric),3),
+cast(eps.insurance_score as numeric) - mod(cast(eps.insurance_score as numeric),5)
 ,count(*) as active_count
 from dw_prod_extracts.ext_policy_snapshots eps
 left join dw_prod.dim_policies using(policy_id)
