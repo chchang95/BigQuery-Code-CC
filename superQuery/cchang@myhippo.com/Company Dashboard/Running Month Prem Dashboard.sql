388ec4e08bf5a6e0436b68349956ec5e42d4f925
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-26 11:54:46 -0700

    Auto-commit: Mon Oct 26 2020 11:54:34 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Company Dashboard/Running Month Prem Dashboard.sql
@@ -0,0 +1,29 @@
+    select 
+        epud.policy_id
+        , lower(state) as state
+        , lower(carrier) as carrier
+        , lower(product) as product
+        , extract(year from date_calendar_month_accounting_basis) as calendar_year
+        , date_calendar_month_accounting_basis as date_accounting_start
+        , date_sub(date_add(date_calendar_month_accounting_basis, INTERVAL 1 MONTH), INTERVAL 1 DAY) as date_accounting_end
+        , reinsurance_treaty_property_accounting
+        , org_id as organization_id
+        , channel
+        , case when renewal_number = 0 then "New" else "Renewal" end as tenure
+        , date_trunc(date_effective, MONTH) as term_effective_month
+        , case when state = 'tx' and calculated_fields_cat_risk_class = 'referral' then 'cat referral' 
+              when calculated_fields_non_cat_risk_class is null or date_effective <= '2020-05-01' then 'not_applicable'
+              else calculated_fields_non_cat_risk_class end as rated_uw_action
+        ,sum(earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl
+        ,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
+        ,sum(written_exposure) as written_exposure
+        ,sum(earned_exposure) as earned_exposure
+        ,sum(written_policy_fee) as written_policy_fee
+        ,sum(earned_policy_fee) as earned_policy_fee
+from dw_prod_extracts.ext_today_knowledge_policy_monthly_premiums epud
+    left join (select policy_id, policy_number, case when organization_id is null then 0 else organization_id end as org_id, channel from dw_prod.dim_policies) dp on epud.policy_id = dp.policy_id
+    left join (select policy_id, calculated_fields_non_cat_risk_class, calculated_fields_cat_risk_class from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-10-25') eps on eps.policy_id = epud.policy_id
+        where date_knowledge = '2020-10-25'
+        and carrier <> 'canopius'
+        -- and product <> 'HO5'
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13
