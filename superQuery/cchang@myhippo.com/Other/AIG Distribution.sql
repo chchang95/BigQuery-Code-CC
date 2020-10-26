156fee6ee2db8118bd05247c00d5c0665f1e0702
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-25 22:29:27 -0700

    Auto-commit: Sun Oct 25 2020 22:29:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Other/AIG Distribution.sql
+++ superQuery/cchang@myhippo.com/Other/AIG Distribution.sql
@@ -2,7 +2,7 @@ select
 eps.state,
 eps.product,
 date_snapshot,
-date_trunc(date_policy_effective, MONTH) as eff_month,
+-- date_trunc(date_policy_effective, MONTH) as eff_month,
 case when eps.renewal_number = 0 then 'new' else 'renewal' end as tenure,
 channel, 
 cast(eps.coverage_a as numeric) - mod(cast(eps.coverage_a as numeric),10000) as coverage_a_banded,
@@ -14,5 +14,5 @@ left join dw_prod.dim_policies using(policy_id)
 where status = 'active'
 and date_snapshot between '2019-01-31' and '2020-09-30'
 and extract(day from DATE_ADD(date_snapshot, interval 1 day)) = 1
-group by 1,2,3,4,5,6,7,8
+group by 1,2,3,4,5,6,7
 order by 1,2,3
