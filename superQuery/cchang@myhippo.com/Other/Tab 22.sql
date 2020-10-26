7a0411e6c93bd52c7246b3870814d27dc0b8a91a
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-25 22:24:52 -0700

    Auto-commit: Sun Oct 25 2020 22:24:39 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/cchang@myhippo.com/Other/Tab 22.sql
@@ -0,0 +1,28 @@
+select 
+eps.state,
+eps.product,
+date_snapshot,
+date_trunc(date_policy_effective, MONTH) as eff_month,
+case when eps.renewal_number = 0 then 'new' else 'renewal' end as tenure,
+channel, 
+eps.coverage_a,
+eps.calculated_fields_age_of_home,
+eps.insurance_score
+-- ,sum(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line) as total_WP
+-- ,sum(written_exposure) as total_WE
+,count(*) as active_count
+from dw_prod_extracts.ext_policy_snapshots eps
+left join dw_prod.dim_policies using(policy_id)
+where status = 'active'
+and date_snapshot between '2018-01-31' and '2020-09-30'
+-- and date_snapshot = '2020-08-31'
+and extract(day from DATE_ADD(date_snapshot, interval 1 day)) = 1
+-- and date_policy_effective >= '2020-07-31'
+-- and eps.product <> 'HO5'
+-- and eps.renewal_number > 0
+-- and channel
+-- and eps.product = 'ho3'
+-- and eps.carrier = 'topa'
+-- and eps.state = 'ca'
+group by 1,2,3,4,5,6,7,8,9
+order by 1,2
