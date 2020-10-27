dd52ddc82510102f374592aa8413b45c0a6e13ee
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-26 19:51:48 -0700

    Auto-commit: Mon Oct 26 2020 19:51:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Tab 9.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Tab 9.sql
@@ -1,5 +1,5 @@
-select carrier, count(distinct policy_id)
-,written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line
+select carrier, count(distinct policy_id) as PIF_count
+,sum(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line) as PIF
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(policy_id)
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
