b98557c8fc2ecca15db3ebafdf133fb126a9c4c0
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-10-26 19:46:04 -0700

    Auto-commit: Mon Oct 26 2020 19:45:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/GC IF Report.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/GC IF Report.sql
@@ -53,12 +53,12 @@ select eps.policy_id
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(policy_id)
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
-where date_snapshot = '2020-10-15'
+where date_snapshot = '2020-09-30'
 -- and carrier <> 'Canopius'
 -- and product <> 'ho5'
 and status = 'active'
 -- and carrier = 'spinnaker'
-and state = 'ca'
+-- and state = 'ca'
 -- and state = 'tx'
 -- and calculated_fields_wind_exclusion <> 'true'
 -- and date_policy_effective <= '2020-05-31'
