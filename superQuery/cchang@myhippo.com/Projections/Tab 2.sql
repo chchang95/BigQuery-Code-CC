5e49847c7b70e7326361ac36d26dd5e1b9d527c3
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-11-09 22:00:55 -0800

    Auto-commit: Mon Nov 09 2020 22:00:39 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/Projections/Tab 2.sql
+++ superQuery/cchang@myhippo.com/Projections/Tab 2.sql
@@ -1,46 +1,74 @@
-with claims_supp as (
-select mon.*
-, case when cc.cat_ind is true then 'Y'
-    when cc.cat_ind is false then 'N'
-    when peril = 'wind' or peril = 'hail' then 'Y'
-    when cat_code is not null then 'Y'
-        else 'N' end as CAT
-, date_trunc(date_effective, MONTH) as policy_effective_month
-, coalesce(loss_paid,0) + coalesce(loss_net_reserve,0) + coalesce(expense_paid,0) + coalesce(expense_net_reserve,0) - coalesce(recoveries,0) as total_incurred_calc
-, coalesce(expense_paid,0) + coalesce(expense_net_reserve,0) as expense_incurred_calc
-from dw_prod_extracts.ext_all_claims_combined mon
-left join dbt_cchin.cat_coding_20201031 cc on mon.claim_number = cc.claim_number
-where carrier <> 'canopius'
+with loss as (
+SELECT
+    mon.month_knowledge,
+    mon.carrier,
+    mon.state,
+    mon.product,
+    month_of_loss,
+    maturity,
+    sum(total_calculated_net_paid_delta_this_month) as incremental_paid,
+    sum(total_net_reserves_delta_this_month) as incremental_reserves,
+    sum(total_incurred_delta_this_month) as incremental_incurred,
+    sum(total_incurred_inception_to_date) as cumulative_incurred
+  FROM
+    dw_prod_extracts.ext_claim_monthly mon
+    left join (select claim_number, reinsurance_treaty from dw_prod_extracts.ext_claims_inception_to_date where date_knowledge = '2020-10-31') USING(claim_number)
+  where is_ebsl is false
+  and carrier <> 'canopius'
+  group by 1,2,3,4,5,6
+ )
+, premium as (
+    select 
+        epud.policy_id
+        , lower(state) as state
+        , lower(carrier) as carrier
+        , lower(product) as product
+        , extract(year from date_calendar_month_accounting_basis) as calendar_year
+        , date_calendar_month_accounting_basis as date_accounting_start
+        , date_sub(date_add(date_calendar_month_accounting_basis, INTERVAL 1 MONTH), INTERVAL 1 DAY) as date_accounting_end
+        , reinsurance_treaty_property_accounting
+        , org_id as organization_id
+        , channel
+        , case when renewal_number = 0 then "New" else "Renewal" end as tenure
+        , date_trunc(date_effective, MONTH) as term_effective_month
+        , case when state = 'tx' and calculated_fields_cat_risk_class = 'referral' then 'cat referral' 
+              when calculated_fields_non_cat_risk_class is null or date_effective <= '2020-05-01' then 'not_applicable'
+              else calculated_fields_non_cat_risk_class end as rated_uw_action
+        ,sum(earned_base + earned_total_optionals + earned_policy_fee - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl
+        ,sum(written_base + written_total_optionals + written_policy_fee - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl
+        ,sum(written_exposure) as written_exposure
+        ,sum(earned_exposure) as earned_exposure
+        ,sum(written_policy_fee) as written_policy_fee
+        ,sum(earned_policy_fee) as earned_policy_fee
+from dw_prod_extracts.ext_policy_monthly_premiums epud
+    left join (select policy_id, policy_number, case when organization_id is null then 0 else organization_id end as org_id, channel from dw_prod.dim_policies) dp on epud.policy_id = dp.policy_id
+    left join (select policy_id, calculated_fields_non_cat_risk_class, calculated_fields_cat_risk_class from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-10-31') eps on eps.policy_id = epud.policy_id
+        where date_knowledge = '2020-10-31'
+        and carrier <> 'canopius'
+        -- and product <> 'HO5'
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13
 )
 , aggregated as (
-SELECT
--- mon.claim_number,
-    mon.date_knowledge as as_of_month,
-    policy_effective_month,
-    mon.carrier,
-    mon.property_data_address_state as state,
-    mon.product,
-    accident_month as accident_month,
-    maturity,
-    CAT
-    ,reinsurance_treaty
-    ,peril
-    -- ,case when reinsurance_treaty = 'Spkr20_Classic' then 'Spkr19_GAP' else reinsurance_treaty end as original_treaty
-    ,sum(loss_paid) as loss_paid
-    ,sum(expense_paid) as expese_paid
-    ,sum(case when claim_closed_no_total_payment is true then 0 else 1 end) as Reported_Claim_Count_Excl_Closed_No_Pay
-    ,sum(expense_incurred_calc) as ALAE_cumulative
-    ,sum(total_incurred_calc) as Incurred_Loss_and_ALAE_Cumulative
-    ,sum(case when total_incurred_calc >= 100000 and CAT = 'N' 
-        then 100000 else total_incurred_calc end) as Incurred_Loss_and_ALAE_Capped_At_100k_NonCat_Only_Cumulative
-    ,sum(case when total_incurred_calc >= 100000 and CAT = 'N' 
-        then total_incurred_calc - 100000 else 0 end) as Incurred_Loss_and_ALAE_Excess_of_100k_NonCat_Only_Cumulative
-  FROM
-    claims_supp mon
-  where is_ebsl is false
-  and mon.date_knowledge <= '2020-10-31'
-  and product <> 'ho5'
-  group by 1,2,3,4,5,6,7,8,9,10
-  )
- select *
- from aggregated
+    select 
+coalesce(date_accounting_start,month_knowledge),
+coalesce(p.carrier, l.carrier),
+coalesce(p.state, l.state),
+coalesce(p.product, l.product)
+,sum(cumulative_incurred) as cumulative_incurred
+,sum(incremental_incurred) as incremental_incurred
+,sum(written_prem_x_ebsl) as written_prem_x_ebsl
+,sum(earned_prem_x_ebsl) as earned_prem_x_ebsl_inc_policy_fees
+,sum(written_exposure) as written_exposure
+,sum(earned_exposure) as earned_exposure
+,sum(written_policy_fee) as written_policy_fee
+,sum(earned_policy_fee) as earned_policy_fee
+from premium p
+full join loss l ON
+p.date_accounting_start = l.month_knowledge AND
+p.date_accounting_start = l.month_of_loss AND
+p.carrier = l.carrier AND
+p.state = l.state AND
+p.product = l.product
+group by 1,2,3,4
+)
+select * from aggregated
