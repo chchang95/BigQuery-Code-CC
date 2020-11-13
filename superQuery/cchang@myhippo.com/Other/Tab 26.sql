130da3ca69f5f7d715b5558310568866856690b6
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-11-12 21:39:30 -0800

    Auto-commit: Thu Nov 12 2020 21:39:17 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/cchang@myhippo.com/Other/Tab 26.sql
@@ -0,0 +1,10 @@
+with policies as (
+select policy_id, policy_number, state, product, carrier, date_policy_effective, status
+from dw_prod_extracts.ext_policy_snapshots
+where state = 'tx'
+and product = 'ho3'
+and status = 'active'
+)
+select efg.*, p.* from dw_prod_extracts.ext_factor_grids efg
+left join policies p using(policy_id)
+where p.policy_id is not null
