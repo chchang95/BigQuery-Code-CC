f05ee34718864db83578cf4328526993645f278e
Chin Chang <67664723+chchang95@users.noreply.github.com>
2020-11-17 16:22:49 -0800

    Auto-commit: Tue Nov 17 2020 16:22:34 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
+++ superQuery/cchang@myhippo.com/Daily Premium and Loss/Daily Loss Ratio.sql
@@ -8,7 +8,7 @@ select state
 ,date_trunc(date_effective, MONTH) as policy_effective_month
 ,case when renewal_number = 0 then "New" else "Renewal" end as tenure
 ,date_trunc(ph.date_first_effective, MONTH) as original_effective_month
-,sum(written_base + written_total_optionals  - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl_x_pol_fee
+,sum(written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line) as written_prem_x_ebsl_x_pol_fee
 ,sum(earned_base + earned_total_optionals - earned_optionals_equipment_breakdown - earned_optionals_service_line) as earned_prem_x_ebsl_x_pol_fee
 ,sum(written_exposure) as written_exposure
 ,sum(earned_exposure) as earned_exposure
@@ -16,7 +16,7 @@ from dw_prod_extracts.ext_today_knowledge_policy_monthly_premiums mon
 left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = mon.policy_id
 left join dw_prod.dim_policy_groups ph on dep.policy_group_number = ph.policy_group_number
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
-where date_knowledge = '2020-10-21'
+where date_knowledge = '2020-11-16'
 and carrier <> 'canopius'
 group by 1,2,3,4,5,6,7,8,9
 )
@@ -29,10 +29,10 @@ select *
         else 'N' end as CAT
 from dw_prod_extracts.ext_claims_inception_to_date cd
 left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on cd.policy_id = dp.policy_id
-left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-10-17') eps on eps.policy_id = cd.policy_id
+left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-11-16') eps on eps.policy_id = cd.policy_id
 left join (select policy_id, policy_number, policy_group_number from dw_prod.dim_policies) dep on dep.policy_id = cd.policy_id
 left join dw_prod.dim_policy_groups ph on dep.policy_group_number = ph.policy_group_number
-  WHERE date_knowledge = '2020-10-22'
+  WHERE date_knowledge = '2020-11-16'
   and carrier <> 'canopius'
 )
 , claims as (
@@ -121,7 +121,7 @@ accounting_treaty
 , round(sum(cat_claim_count) / sum(earned_exposure),3) as cat_frequency
 from combined
 where 1=1
-and accident_month = '2020-09-01'
+and accident_month = '2020-11-01'
 and earned_exposure <> 0
 -- and tenure = 'New'
 -- and original_effective_month <= '2020-09-01'
