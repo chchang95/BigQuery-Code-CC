006eecc947782ba25e647549ca170adcc10f2a29
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-01-26 22:02:33 -0800

    Auto-commit: Tue Jan 26 2021 21:56:20 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/UW Model/Quote Counts 2.0.sql
+++ superQuery/cchang@myhippo.com/UW Model/Quote Counts 2.0.sql
@@ -33,11 +33,11 @@ with quotes_supp as (
 SELECT
     --   q.policy_number,
     --   q.policy_id,
-    --   cast(q.date_quote_first_seen as DATE) as quote_date
-      date_trunc(cast(q.date_quote_first_seen as DATE), WEEK) as quote_week
+      cast(q.date_quote_first_seen as DATE) as quote_date
+      ,date_trunc(cast(q.date_quote_first_seen as DATE), WEEK) as quote_week
       ,date_trunc(cast(q.date_quote_first_seen as DATE), MONTH) as quote_month
-    --   ,qs.org_name as organization_name
-    --   ,q.organization_id
+      ,qs.org_name as organization_name
+      ,q.organization_id
       ,q.state
       ,q.product
       ,q.carrier
@@ -59,23 +59,28 @@ SELECT
     --   ,q.deductible
     --   ,q.wind_deductible
     --   ,q.year_roof_built
-      ,q.insurance_score
+    --   ,q.insurance_score
     --   ,q.non_cat_risk_score
     --   ,q.cat_risk_score
       ,q.non_cat_risk_class
       ,q.cat_risk_class
+      ,q.ready_for_risk_score
     --   ,coalesce(q.non_cat_risk_class, 'not_applicable') as UW_Action
       ,case when q.non_cat_risk_class is null then 'not_applicable'
-      when q.state = 'TX' and q.cat_risk_class = 'referral' then 'referral'
+      when q.state = 'tx' and q.cat_risk_class = 'referral' then 'cat_referral'
       else q.non_cat_risk_class end as UW_Class_with_TX
-      , case when q.ready_for_risk_score is null and q.non_cat_risk_class = 'referral' then 'referral_no_message' 
- when q.ready_for_risk_score = 'true' and q.non_cat_risk_class = 'referral' then 'referral_saw_message'
- else q.non_cat_risk_class end as upd_non_cat_risk_class
+      , case when q.non_cat_risk_class is null then 'not_applicable'
+        when q.state = 'tx' and q.cat_risk_class = 'referral' and q.ready_for_risk_score = 'true' then 'cat_referral_saw_message'
+        when q.state = 'tx' and q.cat_risk_class = 'referral' and q.ready_for_risk_score is null then 'cat_referral_no_message'
+        when q.ready_for_risk_score is null and q.non_cat_risk_class = 'referral' then 'referral_no_message' 
+        when q.ready_for_risk_score = 'true' and q.non_cat_risk_class = 'referral' then 'referral_saw_message'
+        else q.non_cat_risk_class end as upd_non_cat_risk_class
+    ,coalesce(zips.status,'Open') as tx_moratorium 
     --   ,case when coalesce(q.date_bound, cast(q.date_quote_first_seen as date)) <= '2020-04-29' then 'not_applicable'
     --   when q.non_cat_risk_class = 'exterior_inspection_required' or q.non_cat_risk_class = 'interior_inspection_required' or q.non_cat_risk_class = 'referral' then 'rocky'
     --   when q.non_cat_risk_class = 'no_action' then 'happy'
     --   else 'not_applicable' end as UW_Path
---       ,q.date_bound
+      ,q.date_bound
       ,date_trunc(cast(q.date_bound as DATE), WEEK) as bound_week
       ,date_trunc(cast(q.date_bound as DATE), MONTH) as bound_month
       ,q.channel
@@ -87,8 +92,9 @@ SELECT
             LEFT JOIN quotes_supp qs using (quote_id)
             LEFT JOIN dw_prod.dim_policies dp on (q.policy_number = dp.policy_number)
             left join (select policy_id, property_data_roof_type from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-12-08') ps on q.policy_id = ps.policy_id
-      where q.date_quote_first_seen >= '2020-05-01'
-    --   and q.state = 'tx'
+            left join dw_prod.tx_moratorium_zips zips on safe_cast(q.zip_code as numeric) = safe_cast(zips.zip_code as numeric)
+      where q.date_quote_first_seen >= '2020-09-01'
+      and q.state = 'tx'
       and q.product <> 'ho5'
       and q.carrier <> 'canopius'
-      group by 1,2,3,4,5,6,7,8,9,10,11,12,13
+      group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
