27c9ea59233df83bc9e6b2e4de51ee896e8540c9
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-15 09:25:25 -0800

    Auto-commit: Mon Feb 15 2021 09:25:18 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/Policy List.sql
+++ superQuery/cchang@myhippo.com/CC/Policy List.sql
@@ -13,11 +13,16 @@ select eps.policy_id
 ,state 
 ,product 
 ,property_data_protection_class
-,JSON_EXTRACT_SCALAR(property_data_zillow,'$.zestimate') as zillow_market_value
-,property_data_zillow
-,calculated_fields_market_value_much_below_rce
-,prefilled_rebuilding_cost
--- ,JSON_EXTRACT_SCALAR(prefilled_zillow
+,prefilled_fireline_score
+,written_base + written_total_optionals + written_policy_fee as written_total
+,coverage_a as cov_a
+,coverage_b as cov_b
+,coverage_c as cov_c
+,coverage_d as cov_d
+-- ,JSON_EXTRACT_SCALAR(property_data_zillow,'$.zestimate') as zillow_market_value
+-- ,property_data_zillow
+-- ,calculated_fields_market_value_much_below_rce
+-- ,prefilled_rebuilding_cost
 -- ,zestim
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(policy_id)
@@ -25,7 +30,7 @@ left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.p
 where date_snapshot = '2020-12-31'
 -- and date_policy_effective >= '2020-07-01'
 -- and carrier <> 'Canopius'
-and product <> 'ho5'
+and product = 'ho5'
 -- and status = 'active'
 -- and carrier = 'spinnaker'
 -- and state = 'ca'
@@ -34,5 +39,6 @@ and product <> 'ho5'
 -- and calculated_fields_wind_exclusion <> 'true'
 -- and date_policy_effective <= '2020-05-31'
 )
-select count(*) from pol
+-- select count(*) from pol
+select * from pol
 -- where policy_number = 'HAZ-1348250-00'
