47681525f28634d763f81e64e7293d1fddd33dbe
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-16 10:21:45 -0800

    Auto-commit: Tue Feb 16 2021 10:21:39 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/TIV by .sql
+++ superQuery/cchang@myhippo.com/CC/TIV by .sql
@@ -11,17 +11,19 @@ case when renewal_number > 0 then 'renewal' else 'new' end as tenure,
 -- sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
 -- sum(written_base + written_total_optionals) as total_WP,
 count(eps.policy_id) as total_PIF_count,
-sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as total_TIV
+sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as total_TIV,
+sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) / count(eps.policy_id) as avg_TIV
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, policy_number, channel, attributed_organization_id from dw_prod.dim_policies) dp on eps.policy_id = dp.policy_id
 -- left join (select organization_id, organization_name, root_organization_name, from dw_prod.dim_organization_mappings) org_table on dp.org_id = org_table.organization_id
 -- left join (select date, last_day_of_month from dw_prod.utils_dates where date = date(last_day_of_month)) ud on eps.date_snapshot = date(ud.last_day_of_month)
 where 1=1
-and date_snapshot >= '2018-01-01'
+and date_snapshot = '2020-12-31'
 and extract(day from DATE_ADD(date_snapshot, interval 1 day)) = 1
 -- and ud.date is not null
 -- and carrier = 'spinnaker'
 and product <> 'ho5'
 and status = 'active'
+and state in ('ca', 'tx')
 group by 1,2,3,4,5
 order by 1,2,3,4,5
