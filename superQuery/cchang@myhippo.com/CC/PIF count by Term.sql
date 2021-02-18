7ece70244cb619a21f0b6d576dbca39d7d38febd
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-17 21:06:45 -0800

    Auto-commit: Wed Feb 17 2021 21:06:40 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/PIF count by Term.sql
+++ superQuery/cchang@myhippo.com/CC/PIF count by Term.sql
@@ -3,6 +3,7 @@ select state,
 -- , org_id, organization_name, root_organization_name,
 -- sum(case when renewal_number > 0 then 1 else 0 end) as renewal_count,
 -- sum(case when renewal_number = 0 then 1 else 0 end) as new_business_count,
+min(date_policy_effective) as min_date,
 count(eps.policy_id) as total_PIF_count,
 sum(coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0)) as total_TIV
 from dw_prod_extracts.ext_policy_snapshots eps
