a82e071eddeff948e10cfd232dfb45229c58b630
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-17 18:48:43 -0800

    Auto-commit: Wed Feb 17 2021 18:48:30 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/Claims BDX.sql
@@ -49,7 +49,7 @@ SELECT DISTINCT
   FROM dw_prod_extracts.ext_claims_inception_to_date mon
 --   left join (select claim_id, loss_description, damage_description from dw_prod.fct_claims) fc using (claim_id)
   left join (select policy_id, case when organization_id is null then 0 else organization_id end as org_id from dw_prod.dim_policies) dp on mon.policy_id = dp.policy_id
-  WHERE date_knowledge = '2021-02-12'
+  WHERE date_knowledge = '2021-02-17'
   and carrier <> 'canopius'
 --   and is_ebsl is false
   )
@@ -86,4 +86,4 @@ SELECT DISTINCT
   from x
   where 1=1
 -- and loss_net_reserve > 0 and claim_status = 'closed'
-and report_date is between '2021-01-28' and '2021-01-31'
+and report_date between '2021-01-31' and '2021-03-31'
