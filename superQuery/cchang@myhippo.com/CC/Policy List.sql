11cd741b49eb572a2418aa5da7dc25eea3808125
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-22 20:08:35 -0800

    Auto-commit: Mon Feb 22 2021 20:08:25 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/Policy List.sql
+++ superQuery/cchang@myhippo.com/CC/Policy List.sql
@@ -4,42 +4,42 @@ select eps.policy_id
 -- ,eps.is_status_active
 -- ,date_cancellation
 ,dp.policy_number
-,date_policy_effective as effective_date
-,date_policy_expires as expiration_date
-,property_data_address_street as street
-,property_data_address_city as city
-,property_data_address_zip as zip
-,property_data_address_county as county
-,eps.state 
-,eps.product 
-,calculated_fields_age_of_home as age_of_home
-,coverage_deductible as deductible
--- ,property_data_protection_class
--- ,prefilled_fireline_score
-,written_base + written_total_optionals + written_policy_fee as written_total_premium_inc_pol_fees
-,prefilled_rebuilding_cost as prefilled_rebuilding_cost
-,property_data_rebuilding_cost as property_data_rebuilding_cost
-,property_data_building_quality as building_quality
-,case when property_data_building_quality = 'standard' then 'E'
-when property_data_building_quality = 'premium' then 'A'
-when property_data_building_quality = 'luxury' then 'P'
-else 'Error' end as mapping_building_quality
-,property_data_square_footage as square_foot
-,coalesce(coverage_a,0) as cov_a
-,coalesce(coverage_b,0) as cov_b
-,coalesce(coverage_c,0) as cov_c
-,coalesce(coverage_d,0) as cov_d
-,coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0) as tiv
+-- ,date_policy_effective as effective_date
+-- ,date_policy_expires as expiration_date
+-- ,property_data_address_street as street
+-- ,property_data_address_city as city
+-- ,property_data_address_zip as zip
+-- ,property_data_address_county as county
+-- ,eps.state 
+-- ,eps.product 
+-- ,calculated_fields_age_of_home as age_of_home
+-- ,coverage_deductible as deductible
+-- -- ,property_data_protection_class
+-- -- ,prefilled_fireline_score
+-- ,written_base + written_total_optionals + written_policy_fee as written_total_premium_inc_pol_fees
+-- ,prefilled_rebuilding_cost as prefilled_rebuilding_cost
+-- ,property_data_rebuilding_cost as property_data_rebuilding_cost
+-- ,property_data_building_quality as building_quality
+-- ,case when property_data_building_quality = 'standard' then 'E'
+-- when property_data_building_quality = 'premium' then 'A'
+-- when property_data_building_quality = 'luxury' then 'P'
+-- else 'Error' end as mapping_building_quality
+-- ,property_data_square_footage as square_foot
+-- ,coalesce(coverage_a,0) as cov_a
+-- ,coalesce(coverage_b,0) as cov_b
+-- ,coalesce(coverage_c,0) as cov_c
+-- ,coalesce(coverage_d,0) as cov_d
+-- ,coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0) as tiv
 ,channel
-,written_base
-,written_total_optionals
-,written_optionals_equipment_breakdown
-,written_optionals_service_line
-,written_sum_perils
-,written_policy_fee
-,expense_load_digital
-,region_code
-,coverage_wind_deductible as wind_deductible
+-- ,written_base
+-- ,written_total_optionals
+-- ,written_optionals_equipment_breakdown
+-- ,written_optionals_service_line
+-- ,written_sum_perils
+-- ,written_policy_fee
+-- ,expense_load_digital
+-- ,region_code
+-- ,coverage_wind_deductible as wind_deductible
 
 -- ,JSON_EXTRACT_SCALAR(property_data_zillow,'$.zestimate') as zillow_market_value
 -- ,property_data_zillow
@@ -51,14 +51,14 @@ left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(p
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
 left join (select policy_id, policy_number, channel from dw_prod.dim_quotes) dq on CONCAT(left(eps.policy_number,length(eps.policy_number)-2),'00') = dq.policy_number
 left join dw_prod.map_expense_loads as exp ON eps.state=exp.state and eps.product=exp.product and eps.carrier = exp.carrier
-where date_snapshot = '2021-02-17'
+where date_snapshot = '2021-01-31'
 -- and date_policy_effective >= '2020-07-01'
 -- and carrier <> 'Canopius'
-and eps.product = 'ho3'
-and status = 'active'
+-- and eps.product = 'ho3'
+-- and status = 'active'
 -- and carrier = 'spinnaker'
 -- and state = 'ca'
-and eps.state = 'tx'
+-- and eps.state = 'tx'
 -- and property_data_address_zip = '78332'
 -- and calculated_fields_wind_exclusion <> 'true'
 -- and date_policy_effective <= '2020-05-31'
