da9e01b303903de7c049c80ea7d1fed2b0a0f399
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-02-27 12:02:42 -0800

    Auto-commit: Sat Feb 27 2021 12:02:27 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/Reserving/202102 Mid Month ext_all_claims_combined.sql
+++ superQuery/cchang@myhippo.com/Reserving/202102 Mid Month ext_all_claims_combined.sql
@@ -17,7 +17,7 @@ select mon.*
 ,coalesce(loss_paid,0) + coalesce(loss_net_reserve,0) + coalesce(expense_paid,0) + coalesce(expense_net_reserve,0) - coalesce(recoveries,0) as total_incurred_calc
 from dw_prod_extracts.ext_all_claims_combined mon
 left join dbt_actuaries.claim_cat_coding_20210226  cc on (case when tbl_source = 'topa_tpa_claims' then trim(mon.claim_number,'0') else mon.claim_number end) = cast(cc.claim_number as string)
-left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2021-01-31') using(policy_id)
+left join (select policy_id, renewal_number from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2021-02-26') using(policy_id)
 left join dbt_actuaries.claims_mappings_202012 map on mon.peril = map.string_field_0
 left join (select policy_id, date_first_effective from dw_prod.dim_policies left join dw_prod.dim_policy_groups using (policy_group_id)) using (policy_id)
 where (mon.date_knowledge = last_day(date_trunc(mon.date_knowledge, MONTH),MONTH) or mon.date_knowledge in ('2021-02-26'))
