41c81563f43610c27bcb757cbb1ee1f9798236a0
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-03 15:38:31 -0800

    Auto-commit: Wed Mar 03 2021 15:38:20 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/Reserving/20210228 List for CAT.sql
+++ superQuery/cchang@myhippo.com/Reserving/20210228 List for CAT.sql
@@ -25,7 +25,7 @@ SELECT DISTINCT
   left join (select date, last_day_of_quarter from dw_prod.utils_dates where date = date(last_day_of_quarter)) ud on mon.date_knowledge = date(ud.last_day_of_quarter)
   left join dbt_actuaries.cat_coding_w_loss_20210228 cc on (case when tbl_source = 'topa_tpa_claims' then ltrim(mon.claim_number,'0') else mon.claim_number end) = cast(cc.claim_number as string)
   WHERE 1=1
---   and date_knowledge = '2021-02-28'
+  and date_knowledge = '2021-02-28'
   and carrier <> 'canopius'
 --   and last_day_of_quarter is not null
   )
@@ -76,7 +76,7 @@ SELECT DISTINCT
     --   ,Total_Recoverable_Depreciation    
   from claims_supp
   where 1=1
-  and is_ebsl is false
+--   and is_ebsl is false
 --   and claims_policy_number = 'HMO-0345091-00'
 --   and carrier = 'Topa'
 --   and tbl_source = 'hippo_claims'
