f7b852aa069c8bb26988da0455e3b74f5fda7d3e
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-09 09:29:26 -0800

    Auto-commit: Tue Mar 09 2021 09:29:16 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/CA OL Premium for Mort Analysis.sql
+++ superQuery/cchang@myhippo.com/CC/CA OL Premium for Mort Analysis.sql
@@ -82,7 +82,7 @@ group by 1,2,3,4,5,6,7,8,9,10,11
 from premium p
 where state = 'ca'
 and product <> 'ho5'
-group by 1,2,3,4,5,6,7,8,9,10,11
+group by 1,2,3,4,5,6,7,8,9,10
 )
 , summary as (
     select 
