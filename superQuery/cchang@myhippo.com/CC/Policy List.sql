7fa20feb632eef0b485701c75b5658c1f9755d59
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-10 19:37:56 -0800

    Auto-commit: Wed Mar 10 2021 19:37:41 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/Policy List.sql
+++ superQuery/cchang@myhippo.com/CC/Policy List.sql
@@ -12,7 +12,8 @@ select eps.policy_id
 -- ,property_data_address_county as county
 -- ,eps.state 
 -- ,eps.product 
--- ,calculated_fields_age_of_home as age_of_home
+,calculated_fields_age_of_home as age_of_home
+,insurance_score
 -- ,coverage_deductible as deductible
 -- -- ,property_data_protection_class
 -- -- ,prefilled_fireline_score
@@ -31,34 +32,32 @@ select eps.policy_id
 -- ,coalesce(coverage_d,0) as cov_d
 -- ,coalesce(coverage_a,0) + coalesce(coverage_b,0) + coalesce(coverage_c,0) + coalesce(coverage_d,0) as tiv
 ,channel
+,dq.organization_id
+,o.name as org_name
 -- ,written_base
 -- ,written_total_optionals
 -- ,written_optionals_equipment_breakdown
 -- ,written_optionals_service_line
 -- ,written_sum_perils
 -- ,written_policy_fee
--- ,expense_load_digital
-,(expense_load_digital) as written_expense_load
-,(expense_load_digital*earned_exposure) as earned_expense_load
+-- ,(expense_load_digital) as written_expense_load
+-- ,(expense_load_digital*eps.earned_exposure) as earned_expense_load
 
-,((written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line - expense_load_digital) * coalesce(on_level_factor,1)) as on_leveled_written_prem_x_ebsl_x_pol_fees_x_exp_load
-,((earned_base + earned_total_optionals - earned_optionals_equipment_breakdown - earned_optionals_service_line - expense_load_digital*earned_exposure) * coalesce(on_level_factor,1)) as on_leveled_earned_prem_x_ebsl_x_pol_fees_x_exp_load
+-- ,((written_base + written_total_optionals - written_optionals_equipment_breakdown - written_optionals_service_line - expense_load_digital) * coalesce(on_level_factor,1)) as on_leveled_written_prem_x_ebsl_x_pol_fees_x_exp_load
+-- ,((earned_base + earned_total_optionals - earned_optionals_equipment_breakdown - earned_optionals_service_line - expense_load_digital*earned_exposure) * coalesce(on_level_factor,1)) as on_leveled_earned_prem_x_ebsl_x_pol_fees_x_exp_load
 -- ,region_code
 -- ,coverage_wind_deductible as wind_deductible
 
--- ,JSON_EXTRACT_SCALAR(property_data_zillow,'$.zestimate') as zillow_market_value
--- ,property_data_zillow
--- ,calculated_fields_market_value_much_below_rce
--- ,prefilled_rebuilding_cost
--- ,zestim
+
 from dw_prod_extracts.ext_policy_snapshots eps
 left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(policy_id)
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
-left join (select policy_id, policy_number, channel from dw_prod.dim_quotes) dq on CONCAT(left(eps.policy_number,length(eps.policy_number)-2),'00') = dq.policy_number
+left join (select policy_id, policy_number, channel, organization_id from dw_prod.dim_quotes) dq on CONCAT(left(eps.policy_number,length(eps.policy_number)-2),'00') = dq.policy_number
 left join dw_prod.map_expense_loads as exp ON eps.state=exp.state and eps.product=exp.product and eps.carrier = exp.carrier
-where date_snapshot = '2021-01-31'
+LEFT JOIN dw_prod.dim_organizations o on dq.organization_id = o.organization_id
+where date_snapshot = '2021-02-28'
 -- and date_policy_effective >= '2020-07-01'
--- and carrier <> 'Canopius'
+and eps.carrier <> 'canopius'
 -- and eps.product = 'ho3'
 -- and status = 'active'
 -- and carrier = 'spinnaker'
