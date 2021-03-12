f495e294bd7f0e9b896d142269788dc126671ac4
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-12 13:42:31 -0800

    Auto-commit: Fri Mar 12 2021 13:36:20 GMT-0800 (Pacific Standard Time)

--- superQuery/cchang@myhippo.com/CC/UW Monitoring Dashboard.sql
+++ superQuery/cchang@myhippo.com/CC/UW Monitoring Dashboard.sql
@@ -2,13 +2,11 @@ with claims_supp as (
 SELECT DISTINCT
     mon.*
 --       , is_ebsl
-    , case
-  when is_cat_override is not null then is_cat_override
-  when is_cat_override is null and lag(is_cat_override) over(partition by mon.claim_number, coalesce(major_peril, '') order by date_knowledge) is not null 
-    then lag(is_cat_override) over(partition by mon.claim_number, coalesce(major_peril, '') order by date_knowledge) -- property_data_house_under_construction
-  when peril in ('wind', 'hail') then true
-  else is_cat
-  end as CAT
+    , case when cc.cat_ind is true then 'Y'
+    when cc.cat_ind is false then 'N'
+    when peril = 'wind' or peril = 'hail' then 'Y'
+    when cat_code is not null then 'Y'
+        else 'N' end as CAT
     , case when cc.recoded_loss_date is null then date_of_loss else cc.recoded_loss_date end as recoded_loss_date
     , case when cc.recoded_loss_event is null then 'NA' else cc.recoded_loss_event end as recoded_loss_event
       ,dp.org_id
