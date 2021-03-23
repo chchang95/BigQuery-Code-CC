c5d954c913db5c8dcd70e8e4d6a16c9c381e83b8
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-23 16:24:16 -0700

    Auto-commit: Tue Mar 23 2021 16:24:02 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/UW Model/Quote Counts 4.0.sql
+++ superQuery/cchang@myhippo.com/UW Model/Quote Counts 4.0.sql
@@ -36,18 +36,18 @@ SELECT
     --   cast(q.date_quote_first_seen as DATE) as quote_date,
     --   date_trunc(cast(q.date_quote_first_seen as DATE), WEEK) as quote_week
       date_trunc(cast(q.date_quote_first_seen as DATE), MONTH) as quote_month
-      ,qs.org_name as organization_name
+    --   ,qs.org_name as organization_name
     --   ,q.organization_id
       ,q.state
       ,q.product
     --   ,q.carrier
 --       ,q.is_bulk_quoted
-    --   ,case when q.year_built is null then 'Missing'
-    --   when cast(q.year_built as numeric) >= 2000 then 'Post 2000' 
-    --   when cast(q.year_built as numeric) > 1980 then 'Pre 2000' 
-    --   else 'Pre 1980' end as year_built
-      ,q.zip_code
-      ,q.county
+      ,case when q.year_built is null then 'Missing'
+      when cast(q.year_built as numeric) >= 2000 then 'Post 2000' 
+      when cast(q.year_built as numeric) > 1980 then 'Pre 2000' 
+      else 'Pre 1980' end as year_built
+    --   ,q.zip_code
+    --   ,q.county
     --   ,q.roof_type as quote_roof_type
     --   ,q.prefilled_roof_type as prefilled_roof_type
     --   ,property_data_roof_type as policy_roof_type
@@ -60,9 +60,9 @@ SELECT
     --   ,q.wind_deductible
     --   ,q.year_roof_built
     --   ,q.insurance_score
-    --   ,q.non_cat_risk_score
+      ,q.non_cat_risk_score
     --   ,q.cat_risk_score
-    --   ,q.non_cat_risk_class
+      ,q.non_cat_risk_class
     --   ,q.cat_risk_class
     --   ,q.ready_for_risk_score
     --   ,coalesce(q.non_cat_risk_class, 'not_applicable') as UW_Action
@@ -105,7 +105,7 @@ SELECT
             left join (select policy_id, property_data_roof_type from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-12-08') ps on q.policy_id = ps.policy_id
             left join dw_prod.tx_moratorium_zips zips on safe_cast(q.zip_code as numeric) = safe_cast(zips.zip_code as numeric)
       where q.date_quote_first_seen >= '2020-01-01'
-      and q.state = 'ca'
+    --   and q.state = 'ca'
     --   and q.product <> 'ho5'
       and q.carrier <> 'canopius'
       group by 1,2,3,4,5,6,7,8,9,10,11,12,13
