327175a753931d85ad9e788b01e7056f06c79093
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-03-25 14:38:56 -0700

    Auto-commit: Thu Mar 25 2021 14:38:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/UW Model/Quote Counts 4.0.sql
+++ superQuery/cchang@myhippo.com/UW Model/Quote Counts 4.0.sql
@@ -104,6 +104,7 @@ SELECT
             LEFT JOIN dw_prod.dim_policies dp on (q.policy_number = dp.policy_number)
             left join (select policy_id, property_data_roof_type from dw_prod_extracts.ext_policy_snapshots where date_snapshot = '2020-12-08') ps on q.policy_id = ps.policy_id
             left join dw_prod.tx_moratorium_zips zips on safe_cast(q.zip_code as numeric) = safe_cast(zips.zip_code as numeric)
+            left join dbt_cchin.ca_moratorium_zips_august_2020 ca on q.zip_code = safecast(ca.Zips_to_Shut_Off as string) and upper(q.product) = ca.product
       where q.date_quote_first_seen >= '2020-01-01'
     --   and q.state = 'ca'
     --   and q.product <> 'ho5'
