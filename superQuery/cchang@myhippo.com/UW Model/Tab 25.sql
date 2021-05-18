c365e00e29872d68fba1c9bbf4c889d51aaa438d
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-05-18 16:41:52 -0700

    Auto-commit: Tue May 18 2021 16:41:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/UW Model/Tab 25.sql
+++ superQuery/cchang@myhippo.com/UW Model/Tab 25.sql
@@ -114,6 +114,6 @@ SELECT
       and q.product <> 'ho5'
       and q.carrier <> 'canopius'
     --   and q.state in ('ga','in','md','nv','oh','mo','il')
-      group by 1,2,3,4,5,6,7,8,9,10,11,12
+      group by 1,2,3,4,5,6,7,8,9,10,11,12,13
 )
 select * from final
