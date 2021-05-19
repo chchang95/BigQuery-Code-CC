f303302386dbbc9127714750aca93abe46700f27
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-05-19 13:56:48 -0700

    Auto-commit: Wed May 19 2021 13:56:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Company Dashboard/GC IF Report.sql
+++ superQuery/cchang@myhippo.com/Company Dashboard/GC IF Report.sql
@@ -62,9 +62,9 @@ left join (select policy_id, policy_number from dw_prod.dim_policies) dp USING(p
 left join dw_prod.fct_premium_updates fpu on eps.latest_policy_update_id = fpu.policy_update_id
 left join (select policy_id, case when channel is null then 'Online' else channel end as channel, coalesce(organization_id,0) as org_id from dw_prod.dim_policies) using(policy_id)
 left join dw_prod.dim_organizations do on org_id = do.organization_id
-where date_snapshot = '2021-03-31'
-and carrier <> 'canopius'
-and reinsurance_treaty_property = 'spkr21_core'
+where date_snapshot = '2021-04-30'
+-- and carrier <> 'canopius'
+-- and reinsurance_treaty_property = 'spkr21_core'
 -- and product <> 'ho5'
 and status = 'active'
 -- and carrier = 'spinnaker'
