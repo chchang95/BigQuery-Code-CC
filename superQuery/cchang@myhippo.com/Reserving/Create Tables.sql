13455c9d67d22a832fa112c8d0e4c66d056da590
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-06-29 12:27:00 -0700

    Auto-commit: Tue Jun 29 2021 12:26:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Reserving/Create Tables.sql
+++ superQuery/cchang@myhippo.com/Reserving/Create Tables.sql
@@ -1,6 +1,6 @@
-create table dbt_actuaries.ext_all_claims_combined_20210331 as (
-select * from `dw_prod_extracts.ext_all_claims_combined`
-)
+-- create table dbt_actuaries.ext_all_claims_combined_20210331 as (
+-- select * from `dw_prod_extracts.ext_all_claims_combined`
+-- )
 
 -- drop table dbt_cchin.ext_all_claims_combined_20210131
 
@@ -8,8 +8,10 @@ select * from `dw_prod_extracts.ext_all_claims_combined`
 -- select * from `dw_prod_extracts.ext_policy_monthly_premiums` 
 -- )
 
--- create table dbt_actuaries.ext_today_knowledge_policy_monthly_premiums_20210331 as (
+-- create table dbt_actuaries.ext_today_knowledge_policy_monthly_premiums_20210628 as (
 -- select * from `dw_prod_extracts.ext_today_knowledge_policy_monthly_premiums` 
 -- )
 
 -- drop table dbt_actuaries.ext_today_knowledge_policy_monthly_premiums_20210131
+
+select * from `dw_prod_extracts.ext_today_knowledge_policy_monthly_premiums`
