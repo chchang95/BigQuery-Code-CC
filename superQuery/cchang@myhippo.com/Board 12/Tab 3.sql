255e13efff1b05be40e0a2a1e5fbac0b7c5cfa45
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-06-30 16:40:33 -0700

    Auto-commit: Wed Jun 30 2021 16:40:22 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Board 12/Tab 3.sql
+++ superQuery/cchang@myhippo.com/Board 12/Tab 3.sql
@@ -9,7 +9,6 @@
 --3) offer a renewal, they accept the renewal, then decide to cancel after accepting (flat or midterm cancel renewal)---> new_renewal_flag_cancelled
 --if renewal_cancellation_flat_flag = false and renewal_policy_status = terminated then this policy did have some exposure > 0 i.e. midterm cancellation for renewal
 with top as(SELECT 
-    current_date,
     upd.update_action,
     upd.date_customer_update_made,
     upd.date_update_effective,          
@@ -23,21 +22,21 @@ with top as(SELECT
     s.is_cancellation_flat as original_cancellation_flat_flag,                
     s.date_cancellation as original_date_cancellation,        
     s.policy_number as orig_policy_number,      
-    s.renewal_number as orig_renewalnumber,     
-    snap2.policy_number as policy_number,       
-    snap2.renewal_number as renewal_number,         
-    s.coverage_a as initial_cova,           
+    s.renewal_number as orig_term_number,     
+    snap2.policy_number as renewal_policy_number,       
+    snap2.renewal_number as renewal_term_number,         
+    effsnap.coverage_a as coverage_a_at_orig_policy_effective,        
+    s.coverage_a as coverage_a_at_snapshot_date,
     dp1.channel,              
     dp1.state,                
     dp1.product,              
     dp1.tenure,               
     dp1.carrier,              
-    s.coverage_a as written_coverage_a,
-    s.written_total-s.written_optionals_equipment_breakdown - s.written_optionals_service_line as written_total_ex_ebsl,          
-    s.written_total_optionals,      
-    s.written_total,            
-    s.status as original_policy_status,           
-    snap2.status as renewal_policy_status,                
+    s.written_base + s.written_total_optionals -s.written_optionals_equipment_breakdown - s.written_optionals_service_line as written_total_ex_ebsl_ex_pol_fee         
+    -- s.written_total_optionals,      
+    -- s.written_total,            
+    s.status as policy_status_at_snapshot,           
+    snap2.status as renewal_policy_status_at_snapshot,                
     snap2.cancellation_reason1 as renewal_cancellation,               
     snap2.is_cancellation_flat as renewal_cancellation_flat_flag,             
     snap2.date_cancellation as renewal_date_cancellation,             
@@ -64,31 +63,18 @@ with top as(SELECT
     ren.zip_code,             
     ren.county                
 FROM                
-  dw_prod.dim_policies dp1              
-JOIN                
-  dw_prod_extracts.ext_policy_snapshots s               
-ON              
-  s.policy_id = dp1.policy_id  and s.date_snapshot = current_date               
-LEFT JOIN               
- dw_prod_extracts.ext_policy_snapshots snap2                
-ON              
-  snap2.policy_id = dp1.next_policy_id and snap2.date_snapshot = current_date               
-LEFT JOIN               
-  dw_prod.dim_policies dp2              
-ON              
-  dp1.next_policy_id = dp2.policy_id                
-LEFT JOIN               
- dw_prod.dim_renewals ren               
-ON              
- dp1.next_policy_id=ren.policy_id    
-left join
-    dw_prod.fct_policy_updates upd
-on s.latest_policy_update_id = upd.policy_update_id
+    dw_prod.dim_policies dp1 
+JOIN dw_prod_extracts.ext_policy_snapshots s ON s.policy_id = dp1.policy_id  and s.date_snapshot = current_date          
+left join dw_prod_extracts.ext_policy_snapshots effsnap on effsnap.policy_id = dp1.policy_id and effsnap.date_snapshot = dp1.date_effective
+LEFT JOIN dw_prod_extracts.ext_policy_snapshots snap2 ON snap2.policy_id = dp1.next_policy_id and snap2.date_snapshot = current_date               
+LEFT JOIN dw_prod.dim_policies dp2 ON dp1.next_policy_id = dp2.policy_id                
+LEFT JOIN dw_prod.dim_renewals ren ON dp1.next_policy_id=ren.policy_id    
+left join dw_prod.fct_policy_updates upd on s.latest_policy_update_id = upd.policy_update_id
 WHERE               
-  dp1.timestamp_renewal_offered is not null             
-  --and dp1.state='ca'                
-  and dp1.carrier <> 'canopius'     
-  order by original_date_effective          
+    dp1.timestamp_renewal_offered is not null             
+    --and dp1.state='ca'                
+    and dp1.carrier <> 'canopius'     
+    order by original_date_effective          
 ),mid as (
 select 
 case when renewal_accepted = 0 and original_cancellation_reason is not null then date_diff(date_customer_update_made, date_renewal_offered, day) else null end as days_Delta_original,--how long after seeing the renewal did the client ask us to cancel the original term
@@ -114,7 +100,7 @@ SUM(m.new_renewal_flat_cancelled) as renewal_flat_cancelled_count
 FROM mid m
 GROUP BY 1,2,3,4,5,6
 )
-select * from summary
+select * from top
 -- where days_Delta_original is not null
 --where renewal_accepted = 1
 --and renewal_policy_status = 'terminated'
