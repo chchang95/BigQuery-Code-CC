41a5c0cf45daca5f560a92c5e09c3f64efeb4ef7
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-06-30 17:03:59 -0700

    Auto-commit: Wed Jun 30 2021 17:03:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Board 12/Tab 3.sql
+++ superQuery/cchang@myhippo.com/Board 12/Tab 3.sql
@@ -95,11 +95,11 @@ with top as(SELECT
     -- ren.county                
 FROM                
     dw_prod.dim_policies dp1 
-JOIN dw_prod_extracts.ext_policy_snapshots currsnap1 ON currsnap1.policy_id = dp1.policy_id  and currsnap1.date_snapshot = current_date          
+JOIN dw_prod_extracts.ext_policy_snapshots currsnap1 ON currsnap1.policy_id = dp1.policy_id  and currsnap1.date_snapshot = '2021-06-29'          
 left join dw_prod_extracts.ext_policy_snapshots effsnap1 on effsnap1.policy_id = dp1.policy_id and effsnap1.date_snapshot = dp1.date_effective
 left join dw_prod_extracts.ext_policy_snapshots offersnap1 on offersnap1.policy_id = dp1.policy_id and offersnap1.date_snapshot = date(dp1.timestamp_renewal_offered)
 LEFT JOIN dw_prod.dim_policies dp2 ON dp1.next_policy_id = dp2.policy_id                
-LEFT JOIN dw_prod_extracts.ext_policy_snapshots rensnap2 ON rensnap2.policy_id = dp1.next_policy_id and rensnap2.date_snapshot = (case when dp2.date_effective > current_date then current_date else dp2.date_effective end)        
+LEFT JOIN dw_prod_extracts.ext_policy_snapshots rensnap2 ON rensnap2.policy_id = dp1.next_policy_id and rensnap2.date_snapshot = (case when dp2.date_effective > '2021-06-29' then '2021-06-29' else dp2.date_effective end)        
 left join dw_prod.fct_policy_updates upd on currsnap1.latest_policy_update_id = upd.policy_update_id
 WHERE 1=1
     -- dp1.timestamp_renewal_offered is not null             
