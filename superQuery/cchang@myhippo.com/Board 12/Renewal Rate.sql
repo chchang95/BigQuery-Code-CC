15678f4f855347b3d1a0da4ff71e71458bb6d256
Chin Chang <67664723+chchang95@users.noreply.github.com>
2021-07-06 16:14:51 -0700

    Auto-commit: Tue Jul 06 2021 16:14:38 GMT-0700 (Pacific Daylight Time)

--- superQuery/cchang@myhippo.com/Board 12/Renewal Rate.sql
+++ superQuery/cchang@myhippo.com/Board 12/Renewal Rate.sql
@@ -91,6 +91,6 @@ WHERE 1=1
 select * from top
 where state = 'ca'
 and initial_policy_effective_month = '2020-05-01'
-and renewal_offer_premium_w_pol_fees = 0
+and renewal_effective_premium_w_pol_fees = 0
 and renewal_accepted_flag = 1
 
